e8963a9347c91dca45c7b90d295b66ff5341e08b
Robin Aggleton <robin.aggleton@cern.ch>
2014-11-17 13:03:21 +0100

    Add basic CppUnitTest example.
    Update basic test so now does basic Suite, with TestRunner - essentially all you need for simple tests.
    
    Update basicTest with setUp and tearDown methods.

---
+++ bin/basicTest.cpp
@@ -0,0 +1,92 @@
+#include <memory>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+/**
+ * @brief An example of how to implement CppUnit, since the cookbook is confusing.
+ * @details [long description]
+ */
+
+class BasicTest : public CppUnit::TestCase {
+
+    /**
+     * CppUnit provides a TestSuite class that runs any number of TestCases together.
+     * In this case, you'll want your test class to inherit from CppUnit::TestFixture
+     *
+     * These macros saves a lot of typing - basically registers a TestSuite, and
+     * attaches tests to suite.
+     *
+     * Make sure you put these before the other function declarations, otherwise it'll
+     * complain about setUp() being private.
+     */
+    CPPUNIT_TEST_SUITE( BasicTest );
+    CPPUNIT_TEST( runPassingTest );
+    CPPUNIT_TEST( runFailingTest );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    BasicTest() {};
+
+    void setUp();
+    void tearDown();
+
+    void runPassingTest();
+    void runFailingTest();
+
+private:
+    std::vector<double> v;
+
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( BasicTest, "BasicTest" );
+
+// use setUp() and tearDown() to setup/destroy common objects used for tests
+void BasicTest::setUp() {
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+}
+
+void BasicTest::tearDown() {
+}
+
+void BasicTest::runPassingTest() {
+    CPPUNIT_ASSERT( v.size() == 3 );
+    CPPUNIT_ASSERT( v.at(0) == 1 );
+}
+
+void BasicTest::runFailingTest() {
+    CPPUNIT_ASSERT( v.at(1) == 1 );
+}
+
+
+int main() {
+    /**
+     * You can run your tests simply like this, in which case your test class (BasicTest)
+     * needs to inherit from CppUnit::TestCase
+     */
+    // BasicTest b;
+    // b.runPassingTest();
+    /**
+     * This last test fails when you compile & run - will throw a CppUnit exception e.g.
+     * an instance of 'CppUnit::Exception'
+     * what():  assertion failed
+     * - Expression: 1+2 == 3+1
+     */
+    // b.runFailingTest();
+
+    /**
+     * A TestRunner runs your tests and collects the results. It needs a
+     * CppUnit::TestSuite, which your test class returns via static method suite. This
+     * is all done via the macros used in the class above.
+     */
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest( BasicTest::suite() );
+    runner.run();
+
+    return 0;
+}
