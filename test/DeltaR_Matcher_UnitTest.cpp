2e055c0f8c697bb1067cee24c84a3d226e4abbc8
Robin Aggleton <robin.aggleton@cern.ch>
2014-11-18 23:32:16 +0100

    Rename from MatcherUnitTest to DeltaR_Matcher_UnitTest, do setUp and tearDown better, make print statements optional.

---
+++ test/DeltaR_Matcher_UnitTest.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+#include <memory>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "TLorentzVector.h"
+#include "DeltaR_Matcher.h"
+
+/**
+ * @brief Unit tests for DeltaR_Matcher class & subclasses
+ * @details To build and run, do:
+ * scram b runtests
+ * To turn on/off print statments, change the printStatments bool in setUp()
+ */
+class DeltaR_Matcher_UnitTest : public CppUnit::TestCase {
+
+    /**
+     * CppUnit provides a TestSuite class that runs any number of TestCases together.
+     * In this case, you'll want your test class to inherit from CppUnit::TestFixture
+     *
+     * These macros saves a lot of typing - basically registers a TestSuite, and
+     * attaches tests to suite.
+     *
+     * Make sure you put these before the other function declarations, otherwise it'll
+     * complain about setUp() being private.
+     */
+    CPPUNIT_TEST_SUITE( DeltaR_Matcher_UnitTest );
+    CPPUNIT_TEST( runSimpleMatch );
+    CPPUNIT_TEST( checkMinPtMaxEta );
+    CPPUNIT_TEST( checkDeltaRMax );
+    CPPUNIT_TEST( checkPtOrdering );
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    DeltaR_Matcher_UnitTest() {};
+
+    void setUp();
+    void tearDown();
+
+    // Tests
+    void runSimpleMatch();
+    void checkMinPtMaxEta();
+    void checkDeltaRMax();
+    void checkPtOrdering();
+
+    // Utilities
+    void cleanupObjects(DeltaR_Matcher *matcher,
+                        std::vector<TLorentzVector> refJets,
+                        std::vector<TLorentzVector> L1Jets,
+                        std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs);
+
+    void cleanupObjects(DeltaR_Matcher *matcher,
+                        std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs);
+
+private:
+    bool printStatements;
+    std::vector<TLorentzVector> refJets;
+    std::vector<TLorentzVector> L1Jets;
+    DeltaR_Matcher * matcher;
+    std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs;
+};
+
+// CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( DeltaR_Matcher_UnitTest, "DeltaR_Matcher_UnitTest" );
+
+/**
+ * @brief Setup common objects used for tests, at start of each CPPUNIT_TEST
+ */
+void DeltaR_Matcher_UnitTest::setUp() {
+    printStatements = true;
+}
+
+
+/**
+ * @brief Destroy any common objects used for tests, at end of each CPPUNIT_TEST
+ */
+void DeltaR_Matcher_UnitTest::tearDown() {
+    if (matcher != nullptr) delete matcher;
+    if (refJets.size() != 0) refJets.clear();
+    if (L1Jets.size() != 0) L1Jets.clear();
+    if (pairs.size() != 0) pairs.clear();
+}
+
+
+/**
+ * @brief Runs simple matching test to ensure it should do the basics
+ * @details Should match l1_1 with ref_1, etc. Done in all 4 quadrants of eta-phi space.
+ * No min pT or max eta checks.
+ */
+void DeltaR_Matcher_UnitTest::runSimpleMatch() {
+    TLorentzVector l1_1; l1_1.SetPtEtaPhiM(40, 1, 0.5, 0);
+    TLorentzVector ref_1; ref_1.SetPtEtaPhiM(38, 1.3, 0.46, 0);
+    TLorentzVector l1_2; l1_2.SetPtEtaPhiM(40, 1, -0.5, 0);
+    TLorentzVector ref_2; ref_2.SetPtEtaPhiM(38, 1.3, -0.46, 0);
+    TLorentzVector l1_3; l1_3.SetPtEtaPhiM(40, -1, -0.5, 0);
+    TLorentzVector ref_3; ref_3.SetPtEtaPhiM(38, -1.3, -0.46, 0);
+    TLorentzVector l1_4; l1_4.SetPtEtaPhiM(40, -1, 0.5, 0);
+    TLorentzVector ref_4; ref_4.SetPtEtaPhiM(38, -1.3, 0.46, 0);
+
+    refJets = {ref_1, ref_2, ref_3, ref_4};
+    L1Jets = {l1_1, l1_2, l1_3, l1_4};
+
+    matcher = new DeltaR_Matcher(0.7);
+    matcher->setRefJets(refJets);
+    matcher->setL1Jets(L1Jets);
+    pairs = matcher->produceMatchingPairs();
+    // check we have 4 matching pairs & print out matches
+    if (printStatements) {
+        matcher->printName();
+        matcher->printMatches(pairs);
+    }
+    CPPUNIT_ASSERT( pairs.size() == 4 );
+
+}
+
+
+/**
+ * @brief Check that min pT cut works for ref & L1 jets, and max eta works for both
+ * @details 4 pairs of jets that match in eta/phi, but should all fail on min pT or max Eta
+ */
+void DeltaR_Matcher_UnitTest::checkMinPtMaxEta() {
+    TLorentzVector l1_1; l1_1.SetPtEtaPhiM(1.2, 1, 0.5, 0); // fail min L1 jet pT
+    TLorentzVector ref_1; ref_1.SetPtEtaPhiM(38, 1.3, 0.46, 0);
+
+    TLorentzVector l1_2; l1_2.SetPtEtaPhiM(40, 1, -0.5, 0);
+    TLorentzVector ref_2; ref_2.SetPtEtaPhiM(1.2, 1.3, -0.46, 0); // fail min ref jet pT
+
+    TLorentzVector l1_3; l1_3.SetPtEtaPhiM(40, -5.1, -1, 0); // fail max L1 jet eta
+    TLorentzVector ref_3; ref_3.SetPtEtaPhiM(38, -4.6, -1.2, 0);
+
+    TLorentzVector l1_4; l1_4.SetPtEtaPhiM(40, 4.5, 1.1, 0);
+    TLorentzVector ref_4; ref_4.SetPtEtaPhiM(38, 5.1, 1.2, 0); // fail max ref jet Eta
+
+    refJets = {ref_1, ref_2, ref_3, ref_4};
+    L1Jets = {l1_1, l1_2, l1_3, l1_4};
+
+    // test via constructor
+    matcher = new DeltaR_Matcher(0.7, 3, 4, 5);
+    matcher->setRefJets(refJets);
+    matcher->setL1Jets(L1Jets);
+    pairs = matcher->produceMatchingPairs();
+    if (printStatements) {
+        matcher->printName();
+        matcher->printMatches(pairs);
+    }
+    CPPUNIT_ASSERT( pairs.size() == 0 );
+
+    delete matcher;
+
+    // test via setters
+    matcher = new DeltaR_Matcher(0.7);
+    matcher->setMinL1JetPt(1.5);
+    matcher->setMinRefJetPt(1.5);
+    matcher->setMaxJetEta(5);
+    matcher->setRefJets(refJets);
+    matcher->setL1Jets(L1Jets);
+    pairs = matcher->produceMatchingPairs();
+    if (printStatements) {
+        matcher->printName();
+        matcher->printMatches(pairs);
+    }
+    CPPUNIT_ASSERT( pairs.size() == 0 );
+}
+
+
+/**
+ * @brief Check that DeltaR_Matcher excludes pairs with deltaR > maxDeltaR
+ * @details Check for deltaEta, deltaPhi, and deltaR. No pairs should match.
+ */
+void DeltaR_Matcher_UnitTest::checkDeltaRMax() {
+    TLorentzVector l1_1; l1_1.SetPtEtaPhiM(40, 1, 0.71, 0); // deltaR > maxDeltaR
+    TLorentzVector ref_1; ref_1.SetPtEtaPhiM(38, 1.41, 0.4, 0);
+
+    TLorentzVector l1_2; l1_2.SetPtEtaPhiM(40, 1, -0.5, 0); // deltaEta > maxDeltaR
+    TLorentzVector ref_2; ref_2.SetPtEtaPhiM(38, 1.8, -0.5, 0);
+
+    TLorentzVector l1_3; l1_3.SetPtEtaPhiM(40, -1, -0.5, 0); // deltaPhi > maxDeltaR (checking we calculate out deltaPhi corectly)
+    TLorentzVector ref_3; ref_3.SetPtEtaPhiM(38, -1, -1.46, 0);
+
+    refJets = {ref_1, ref_2, ref_3};
+    L1Jets = {l1_1, l1_2, l1_3};
+
+    matcher = new DeltaR_Matcher(0.5);
+    matcher->setRefJets(refJets);
+    matcher->setL1Jets(L1Jets);
+    pairs = matcher->produceMatchingPairs();
+    if (printStatements) {
+        matcher->printName();
+        matcher->printMatches(pairs);
+    }
+    CPPUNIT_ASSERT( pairs.size() == 0 );
+}
+
+
+/**
+ * @brief Check to ensure that matches are made with pT-ordered collections
+ * @details [long description]
+ */
+void DeltaR_Matcher_UnitTest::checkPtOrdering() {
+    TLorentzVector l1_1;  l1_1.SetPtEtaPhiM(40, 1, 0.5, 0); // ref_1 should match with l1_1, not l1_2
+    TLorentzVector l1_2; l1_2.SetPtEtaPhiM(38, 1, 1.0, 0);
+    TLorentzVector ref_1; ref_1.SetPtEtaPhiM(32, 1, 0.75, 0);
+
+    refJets = {ref_1};
+    L1Jets = {l1_1, l1_2};
+
+    matcher = new DeltaR_Matcher(0.5);
+    matcher->setRefJets(refJets);
+    matcher->setL1Jets(L1Jets);
+    pairs = matcher->produceMatchingPairs();
+    if (printStatements) {
+        matcher->printName();
+        matcher->printMatches(pairs);
+    }
+    CPPUNIT_ASSERT( pairs.size() == 1 );
+    CPPUNIT_ASSERT( pairs.at(0).second == l1_1 );
+}
+
+
+/**
+ * @brief Main routine that runs the tests and output the results to screen.
+ */
+int main() {
+    /**
+     * A TestRunner runs your tests and collects the results. It needs a
+     * CppUnit::TestSuite, which your test class returns via static method suite. This
+     * is all done via the macros used in the class above.
+     */
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest( DeltaR_Matcher_UnitTest::suite() );
+    runner.run();
+    return 0;
+
+    // alternatively, to include in BuildProcess, have to return value
+    // diff to 0 in event of Failure
+    // bool wasSuccessful = runner.run("", false);
+    // return !wasSuccessful;
+}
