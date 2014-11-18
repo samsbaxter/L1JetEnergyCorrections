bbe822ce53886bed9d48346a3ccf8008092065c1
Robin Aggleton <robin.aggleton@cern.ch>
2014-11-18 20:38:02 +0100

    add test for minJetPt and maxJetEta

--- test/matcherUnitTests.cpp
+++ test/matcherUnitTests.cpp
@@ -29,7 +29,8 @@ class MatcherUnitTest : public CppUnit::TestCase {
      * complain about setUp() being private.
      */
     CPPUNIT_TEST_SUITE( MatcherUnitTest );
-    CPPUNIT_TEST( runSimpleTest );
+    CPPUNIT_TEST( runSimpleMatch );
+    CPPUNIT_TEST( checkMinPtMaxEta );
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -38,8 +39,13 @@ public:
     void setUp();
     void tearDown();
 
-    void runSimpleTest();
+    void runSimpleMatch();
+    void checkMinPtMaxEta();
 
+    void cleanupObjects(DeltaR_Matcher *m,
+                        std::vector<TLorentzVector> refJets,
+                        std::vector<TLorentzVector> L1Jets,
+                        std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs);
 private:
     std::vector<TLorentzVector> refJets;
     std::vector<TLorentzVector> L1Jets;
@@ -54,11 +60,25 @@ private:
 void MatcherUnitTest::setUp() {
 }
 
+
 /**
  * @brief Destroy any common objects used for tests
  */
 void MatcherUnitTest::tearDown() {
     // if (m != nullptr) delete m;
+    // if (refJets.size() != 0) refJets.clear();
+    // if (L1Jets.size() != 0) L1Jets.clear();
+}
+
+
+void cleanupObjects(DeltaR_Matcher *m,
+                    std::vector<TLorentzVector> refJets,
+                    std::vector<TLorentzVector> L1Jets,
+                    std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs) {
+    if (m != nullptr) delete m;
+    if (refJets.size() != 0) refJets.clear();
+    if (L1Jets.size() != 0) L1Jets.clear();
+    if (pairs.size() != 0) pairs.clear();
 }
 
 /**
@@ -66,7 +86,7 @@ void MatcherUnitTest::tearDown() {
  * @details Should match l1_1 with ref_1, etc. Done in all 4 quadrants of eta-phi space.
  * No min pT or max eta checks.
  */
-void MatcherUnitTest::runSimpleTest() {
+void MatcherUnitTest::runSimpleMatch() {
     TLorentzVector l1_1; l1_1.SetPtEtaPhiM(40,1,0.5,0);
     TLorentzVector ref_1; ref_1.SetPtEtaPhiM(38,1.3,0.46,0);
     TLorentzVector l1_2; l1_2.SetPtEtaPhiM(40,1,-0.5,0);
@@ -92,6 +112,55 @@ void MatcherUnitTest::runSimpleTest() {
     refJets.clear();
     L1Jets.clear();
 }
+
+
+/**
+ * @brief Check that min pT cut works for ref & L1 jets, and max eta works for both
+ * @details 4 pairs of jets that match in eta/phi, but should fail on min pT or max Eta
+ */
+void MatcherUnitTest::checkMinPtMaxEta() {
+    TLorentzVector l1_1; l1_1.SetPtEtaPhiM(1.2,1,0.5,0); // fail min L1 jet pT
+    TLorentzVector ref_1; ref_1.SetPtEtaPhiM(38,1.3,0.46,0);
+
+    TLorentzVector l1_2; l1_2.SetPtEtaPhiM(40,1,-0.5,0);
+    TLorentzVector ref_2; ref_2.SetPtEtaPhiM(1.2,1.3,-0.46,0); // fail min ref jet pT
+
+    TLorentzVector l1_3; l1_3.SetPtEtaPhiM(40,-5.1,-1,0); // fail max L1 jet eta
+    TLorentzVector ref_3; ref_3.SetPtEtaPhiM(38,-4.6,-1.2,0);
+
+    TLorentzVector l1_4; l1_4.SetPtEtaPhiM(40,4.5,1.1,0);
+    TLorentzVector ref_4; ref_4.SetPtEtaPhiM(38,5.1,1.2,0); // fail max ref jet Eta
+
+    refJets = {ref_1, ref_2, ref_3, ref_4};
+    L1Jets = {l1_1, l1_2, l1_3, l1_4};
+
+    // test via constructor
+    m = new DeltaR_Matcher(0.7, 3, 4, 5);
+    m->setRefJets(refJets);
+    m->setL1Jets(L1Jets);
+    m->printName();
+    std::vector<std::pair<TLorentzVector, TLorentzVector>> pairs = m->produceMatchingPairs();
+    m->printMatches(pairs);
+    CPPUNIT_ASSERT( pairs.size() == 0 );
+    delete m;
+    if (pairs.size() != 0) pairs.clear();
+
+    // test via setters
+    m = new DeltaR_Matcher(0.7);
+    m->setMinL1JetPt(1.5);
+    m->setMinRefJetPt(1.5);
+    m->setMaxJetEta(5);
+    m->printName();
+    pairs = m->produceMatchingPairs();
+    m->printMatches(pairs);
+    CPPUNIT_ASSERT( pairs.size() == 0 );
+
+    delete m;
+    if (pairs.size() != 0) pairs.clear();
+    refJets.clear();
+    L1Jets.clear();
+}
+
 
 /**
  * @brief Main routine that runs the tests and output the results to screen.
