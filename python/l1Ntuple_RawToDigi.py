7dc2f30556271ee1344f32a417b532fd7c43d15f
Robin Aggleton <robin.aggleton@cern.ch>
2015-04-23 18:36:53 +0200

    Update RawToDigi with all the latest greatest hacks.

--- python/l1Ntuple_RawToDigi.py
+++ python/l1Ntuple_RawToDigi.py
@@ -1,11 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 """
-Minimal working example of RawToDigi to demostrate that the output from
-the GCT emulator is different depending on whether it is fed the output from
-the unpacker (gctDigis), or whether you pass the TP digis to the RCT emulator,
-and then send the regions it produces to the GCT emulator (simGctDigisRCT)
-
+Minimal working example of RawToDigi to compare the output from
+the unpacker (gctDigis), and the TPs + RCT emulator + GCT emulator chain.
 """
 
 process = cms.Process("L1NTUPLE")
@@ -24,7 +21,9 @@ process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 200
 process.MessageLogger.suppressWarning = cms.untracked.vstring(
-    "csctfDigis"
+    "csctfDigis",
+    "l1ExtraTreeProducerRCT",
+    "l1ExtraTreeProducer"
     )
 
 process.gctDigis.numberOfGctSamplesToUnpack = cms.uint32(1)
@@ -33,25 +32,68 @@ process.gctDigis.numberOfGctSamplesToUnpack = cms.uint32(1)
 process.simGctDigis.inputLabel = cms.InputTag('gctDigis')
 
 # GCT emulator run from RCT emulator
-process.simRctDigis.hcalDigis = cms.VInputTag(cms.InputTag('hcalDigis'))
+
+# HACK NUMBER 1 & 2 (a la Mambo no. 5)
+# Remake the HCAL TPs since hcalDigis outputs nothing
+# But make sure you use the unsupressed digis, not the hcalDigis
+process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
+process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
+    cms.InputTag('simHcalUnsuppressedDigis'),
+    cms.InputTag('simHcalUnsuppressedDigis')
+)
+# Rerun the RCT emulator using the TPs
+process.simRctDigis.hcalDigis = cms.VInputTag(cms.InputTag('simHcalTriggerPrimitiveDigis'))
 process.simRctDigis.ecalDigis = cms.VInputTag(cms.InputTag('ecalDigis', 'EcalTriggerPrimitives' ))
+# HACK NUMBER 3: setup the RCT config properly.
+# The GlobalTag DOES NOT setup the RCT params - you have to load the cff *manually*
+# At least, as of 23/4/15 this is the case. Check with Maria Cepeda/Mike Mulhearn
+process.load('L1Trigger.L1TCalorimeter.caloStage1RCTLuts_cff')
+# Incase you want to test how the RCT is configured
+# (may need L1TriggerConfig/RCTConfigProducers)
+process.l1RCTParametersTest = cms.EDAnalyzer('L1RCTParametersTester')
+# process.l1RCTChannelMaskTest = cms.EDAnalyzer('L1RCTChannelMaskTester')
+# process.l1RCTOutputScalesTest = cms.EDAnalyzer('L1ScalesTester')
+# process.printGlobalTagL1Rct = cms.Sequence(process.l1RCTParametersTest*process.l1RCTChannelMaskTest*process.l1RCTOutputScalesTest)
+
+# Now rerun the GCT emulator using those re-made regions
+# Wahey, no haxs here.
 process.simGctDigisRCT = process.simGctDigis.clone()
 process.simGctDigisRCT.inputLabel = cms.InputTag('simRctDigis')
 
-process.l1extraParticlesRCT = process.l1extraParticles.clone()
-process.l1extraParticlesRCT.centralJetSource = cms.InputTag("simGctDigisRCT","cenJets")
-process.l1extraParticlesRCT.forwardJetSource = cms.InputTag("simGctDigisRCT","fwdJets")
 
 # L1 ntuple producers
 process.load("L1TriggerDPG.L1Ntuples.l1ExtraTreeProducer_cfi")
+# from the unpacker
 process.l1extraParticles.centralBxOnly = cms.bool(True)
 process.l1ExtraTreeProducerRCT = process.l1ExtraTreeProducer.clone()
+# from the TP->RCT->GCT chain
+process.l1extraParticlesRCT = process.l1extraParticles.clone()
+process.l1extraParticlesRCT.centralJetSource = cms.InputTag("simGctDigisRCT","cenJets")
+process.l1extraParticlesRCT.forwardJetSource = cms.InputTag("simGctDigisRCT","fwdJets")
 process.l1ExtraTreeProducerRCT.cenJetLabel = cms.untracked.InputTag("l1extraParticlesRCT:Central")
 process.l1ExtraTreeProducerRCT.fwdJetLabel = cms.untracked.InputTag("l1extraParticlesRCT:Forward")
+
+##############################
+# New RCT calibs
+##############################
+from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
+process.newRCTConfig = cms.ESSource("PoolDBESSource",
+    CondDBSetup,
+    connect = cms.string('frontier://FrontierPrep/CMS_COND_L1T'),
+    DumpStat=cms.untracked.bool(True),
+    toGet = cms.VPSet(
+        cms.PSet(
+           record = cms.string('L1RCTParametersRcd'),
+           tag = cms.string('L1RCTParametersRcd_L1TDevelCollisions_ExtendedScaleFactorsV1')
+        )
+    )
+)
+process.prefer("newRCTConfig")
 
 
 process.p = cms.Path(
     process.RawToDigi
+    +process.simHcalTriggerPrimitiveDigis
     +process.simGctDigis
     +process.l1extraParticles
     +process.l1ExtraTreeProducer
@@ -59,6 +101,8 @@ process.p = cms.Path(
     +process.simGctDigisRCT
     +process.l1extraParticlesRCT
     +process.l1ExtraTreeProducerRCT
+    +process.l1RCTParametersTest
+    # +process.printGlobalTagL1Rct
 )
 
 
@@ -68,7 +112,7 @@ process.p = cms.Path(
 
 # output file
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('L1Tree.root')
+    fileName = cms.string('L1Tree_test.root')
 )
 
 process.GlobalTag.globaltag = cms.string('PHYS14_ST_V1::All') # for Phys14 AVE30BX50 sample
@@ -85,15 +129,17 @@ process.source = cms.Source ("PoolSource",
 process.output = cms.OutputModule(
     "PoolOutputModule",
     outputCommands = cms.untracked.vstring(
+        # 'keep *'
         'drop *',
         'keep *_gctDigis_*_*',
         'keep *_simGctDigis_*_*',
         'keep *_simRctDigis_*_*',
         'keep *_simGctDigisRCT_*_*',
         'keep *_hcalDigis_*_*',
-        'keep *_ecalDigis_*_*'
+        'keep *_ecalDigis_*_*',
+        'keep *_simHcalUnsuppressedDigis_*_*'
         ),
-    fileName = cms.untracked.string('SimGCTEmulator.root')
+    fileName = cms.untracked.string('SimGCTEmulator_test.root')
     )
 
 process.output_step = cms.EndPath(process.output)
