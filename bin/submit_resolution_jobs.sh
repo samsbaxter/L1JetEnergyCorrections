4bd329c3d85526097bbccc1e3c94b514e65b6837
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-05 15:15:36 +0200

    Add in batch scripts for resolution plots

---
+++ bin/submit_resolution_jobs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# To submit lots of makeResolutionPlots jobs on lxbatch system (or any bsub)
+# Splits it up by eta bin
+#
+# Change pairs variable to full path of pairs file you want to run over
+# The output files from makeResolutionPlots.py will be made in the same directory
+
+
+declare -a etaBins=(
+0
+0.348
+0.695
+1.044
+1.392
+1.74
+2.172
+3.0
+3.5
+4.0
+4.5
+5.001
+) 
+
+pairs="/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_2_0/src/L1Trigger/L1JetEnergyCorrections/QCD_Phys14_AVE30BX50_newRCT_HCALhack_v2/pairs_QCD_Pt-15to1000_Phys14_AVE30BX50_GCT_QCDPhys14_newRCT_HCALhack_v2_GCT_ak5_ref14to1000_l10to500_calibrated_min0.root"
+
+fdir=`dirname $pairs`
+fname=`basename $pairs`
+
+echo "Using pairs file $pairs"
+echo "Writing output to directory: $fdir"
+
+len=${#etaBins[@]}
+len=$(( len - 1 ))
+for ((i=0;i<$len;++i));
+do
+    j=$(( i + 1 ))
+    etamin=${etaBins[i]}
+    etamax=${etaBins[j]}
+    jobname="${etamin}to${etamax}"
+    outname=${fname#pairs_}
+    outname=${outname%.root}
+    outname="res_${outname}_${i}.root"
+    echo "$jobname"
+    echo "$outname"
+    bsub -q 8nh -J $jobname "sh resolution_batch.sh ${fdir}/${fname} ${fdir}/${outname} --excl --etaInd ${i}"
+done
+
+outname=${fname#pairs_}
+outname=${outname%.root}
+bsub -q 8nh -J allCentral "sh resolution_batch.sh ${fdir}/${fname} ${fdir}/res_${outname}_central.root --incl --central"
+bsub -q 8nh -J allForward "sh resolution_batch.sh ${fdir}/${fname} ${fdir}res_${outname}_forward.root --incl --forward"
+bsub -q 8nh -J all "sh resolution_batch.sh ${fdir}/${fname} ${fdir}/res_${outname}_all.root --incl"
