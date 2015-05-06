60026410c0b616f9e51ae150d24cd5dbd40807a3
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-06 18:05:13 +0200

    Central LUT for 720 Phys14 samples

---
+++ python/LUT_720_QCD_Phys14_AVE30BX50_newRCT_HCALhack_v2_central.py
@@ -0,0 +1,21 @@
+# put this in your py config file
+    PFCoefficients = cms.PSet(
+        nonTauJetCalib0 = cms.vdouble(3.018,104.054,4.377,-511.101,0.010,-17.195),
+        nonTauJetCalib1 = cms.vdouble(6.515,55.134,4.429,-75.990,0.006,-16.071),
+        nonTauJetCalib2 = cms.vdouble(2.666,59.403,3.645,-458.280,0.009,-19.257),
+        nonTauJetCalib3 = cms.vdouble(0.716,54.716,2.720,-9009.001,0.010,-24.075),
+        nonTauJetCalib4 = cms.vdouble(1.391,31.512,2.494,-533.649,0.011,-18.943),
+        nonTauJetCalib5 = cms.vdouble(1.569,10.197,1.616,-37.032,0.008,-17.153),
+        nonTauJetCalib6 = cms.vdouble(1.580,9.645,1.844,-53.545,0.008,-18.042),
+        nonTauJetCalib7 = cms.vdouble(1.000,0.000,0.000,0.000,0.000,0.000),
+        nonTauJetCalib8 = cms.vdouble(1.000,0.000,0.000,0.000,0.000,0.000),
+        nonTauJetCalib9 = cms.vdouble(1.000,0.000,0.000,0.000,0.000,0.000),
+        nonTauJetCalib10 = cms.vdouble(1.000,0.000,0.000,0.000,0.000,0.000),
+        tauJetCalib0 = cms.vdouble(3.018,104.054,4.377,-511.101,0.010,-17.195),
+        tauJetCalib1 = cms.vdouble(6.515,55.134,4.429,-75.990,0.006,-16.071),
+        tauJetCalib2 = cms.vdouble(2.666,59.403,3.645,-458.280,0.009,-19.257),
+        tauJetCalib3 = cms.vdouble(0.716,54.716,2.720,-9009.001,0.010,-24.075),
+        tauJetCalib4 = cms.vdouble(1.391,31.512,2.494,-533.649,0.011,-18.943),
+        tauJetCalib5 = cms.vdouble(1.569,10.197,1.616,-37.032,0.008,-17.153),
+        tauJetCalib6 = cms.vdouble(1.580,9.645,1.844,-53.545,0.008,-18.042),
+    )
