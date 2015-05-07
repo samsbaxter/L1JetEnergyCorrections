f0ab8aaa863306a0c049be56497cb85b1fb2242d
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-07 16:19:27 +0200

    add in Phys14 ttbar

--- python/samples.py
+++ python/samples.py
@@ -27,6 +27,9 @@ samples = {
     "TTbarFall13PU20bx25": Dataset(inputDataset='/TT_Tune4C_13TeV-pythia8-tauola/Fall13dr-tsg_PU20bx25_POSTLS162_V2-v1/GEN-SIM-RAW',
                                     unitsPerJob=100),
 
+    "TTbarPhys14AVE30BX50": Dataset(inputDataset='/TT_Tune4C_13TeV-pythia8-tauola/Phys14DR-AVE30BX50_tsg_PHYS14_ST_V1-v1/GEN-SIM-RAW',
+                                    unitsPerJob=50),
+
     "QCDFlatSpring14": Dataset(inputDataset='/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Spring14dr-Flat20to50_POSTLS170_V5-v1/GEN-SIM-RAW',
                                 unitsPerJob=50),
 
