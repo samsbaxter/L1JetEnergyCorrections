27ff2732bc7dda2a54d7cd85e485633940441213
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-22 12:02:05 +0200

    Add in Spring15 QCD samples

--- python/samples.py
+++ python/samples.py
@@ -42,6 +42,11 @@ ptbins = [15, 30, 50, 80, 120, 170, 300, 470, 600, 800, 1000, 1400, 1800]
 for i, ptmin in enumerate(ptbins[:-1]):
     ptmax = ptbins[i+1]
 
+    # Spring15 AVEPU30 50ns
+    key = "QCD_Pt-%dto%d_Spring15_AVE30BX50"
+    samples[key] = Dataset(inputDataset="/QCD_Pt_%dto%d_TuneCUETP8M1_13TeV_pythia8/RunIISpring15Digi74-AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/GEN-SIM-RAW" % (ptmin, ptmax),
+                            unitsPerJob=50, totalUnits=-1)
+
     # Phys14 AVEPU20 25ns
     key = "QCD_Pt-%dto%d_Phys14_AVE20BX25" % (ptmin, ptmax)
     samples[key] = Dataset(inputDataset="/QCD_Pt-%dto%d_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW" % (ptmin, ptmax),
@@ -71,6 +76,7 @@ for i, ptmin in enumerate(ptbins[:-1]):
                             unitsPerJob=50, totalUnits=-1)
 
 # the last 1800 to inf ones
+# note that starting with Spring15, they extended the pt bins, so 1800-2400, 2400-3200, 3200toInf
 samples["QCD_Pt-1800_Phys14_AVE20BX25"] = Dataset(inputDataset="/QCD_Pt-1800_Tune4C_13TeV_pythia8/Phys14DR-AVE20BX25_tsg_castor_PHYS14_25_V3-v1/GEN-SIM-RAW",
                                                     unitsPerJob=50, totalUnits=-1)
 samples["QCD_Pt-1800_Phys14_AVE30BX50"] = Dataset(inputDataset="/QCD_Pt-1800_Tune4C_13TeV_pythia8/Phys14DR-AVE30BX50_tsg_castor_PHYS14_ST_V1-v1/GEN-SIM-RAW",
@@ -83,6 +89,7 @@ samples["QCD_Pt-1800_Fall13_PU40bx50"] = Dataset(inputDataset="/QCD_Pt-1800_Tune
                                                     unitsPerJob=150, totalUnits=-1)
 
 # adhoc mini samples for diff QCD sets
+samples_qcd_Spring15_AVE20BX50 = dict((k, samples[k]) for k in samples.keys() if re.match(r"QCD_Pt-[\dto]*_Spring15_AVE30BX50", k))
 samples_qcd_Phys14_AVE20BX25 = dict((k, samples[k]) for k in samples.keys() if re.match(r"QCD_Pt-[\dto]*_Phys14_AVE20BX25", k))
 samples_qcd_Phys14_AVE30BX50 = dict((k, samples[k]) for k in samples.keys() if re.match(r"QCD_Pt-[\dto]*_Phys14_AVE30BX50", k))
 samples_qcd_Fall13_PU20bx25 = dict((k, samples[k]) for k in samples.keys() if re.match(r"QCD_Pt-[\dto]*_Fall13_PU20bx25", k))
