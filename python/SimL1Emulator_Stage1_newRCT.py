110e23e041f577c91168ac1df0a58a75de05210c
Robin Aggleton <robin.aggleton@cern.ch>
2015-06-08 14:24:46 +0200

    An update to the Stage 1 config, with new RCT and reorganised so more like the GCT one

--- python/SimL1Emulator_Stage1_newRCT.py
+++ python/SimL1Emulator_Stage1_newRCT.py
@@ -1,107 +1,62 @@
+"""
+Config file to run the Stage 1 emulator to make Ntuples.
+
+Stores internal jets, as well as ak5 and ak4 GenJets
+
+YOU MUST RUN WITH CMSSW 742 OR NEWER TO PICK UP THE NEW RCT CALIBS.
+
+"""
+
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('L1TEMULATION')
+# To use new RCT calibrations (default in samples is 2012):
+new_RCT_calibs = True
+
+# To save the EDM content as well:
+# (WARNING: DON'T do this for big production - will be HUGE)
+save_EDM = True
+
+# Global tag (note, you must ensure it matches input file)
+gt = 'PHYS14_25_V3'  # for Phys14 AVE30BX50 sample
+
+# Things to append to L1Ntuple/EDM filename
+# (if using new RCT calibs, this gets auto added)
+file_append = "_Stage1"
+
+# Add in a filename appendix here for your GlobalTag if you want.
+if gt == 'PHYS14_25_V3':
+    file_append += "_Phys14"
+else:
+    file_append += "_" + gt
+
+###################################################################
+process = cms.Process('L1NTUPLE')
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.Geometry.GeometryIdeal_cff')
+# process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
 
 # Select the Message Logger output you would like to see:
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 200
 process.MessageLogger.suppressWarning = cms.untracked.vstring(
-                                            "l1ExtraTreeProducerGenAk4",
-                                            "l1ExtraTreeProducerGenAk5",
-                                            "l1ExtraTreeProducerIntern"
-                                            )
-
-##############################
-# Input/output & standard stuff
-##############################
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-    )
-
-# Input source
-readFiles = cms.untracked.vstring()
-process.source = cms.Source("PoolSource",
-    # secondaryFileNames = cms.untracked.vstring(),
-    # fileNames = cms.untracked.vstring("root://xrootd.unl.edu//store/mc/Fall13dr/Neutrino_Pt-2to20_gun/GEN-SIM-RAW/tsg_PU40bx25_POSTLS162_V2-v1/00005/02B79593-F47F-E311-8FF6-003048FFD796.root")
-    # fileNames = cms.untracked.vstring("root://xrootd.unl.edu//store/mc/Spring14dr/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/GEN-SIM-RAW/Flat20to50_POSTLS170_V5-v1/00000/008B2415-EBDD-E311-B807-20CF3027A564.root")
-    # fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_2_0_pre7/src/L1TriggerDPG/L1Ntuples/test/QCD_GEN_SIM_RAW.root')
-    # fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_2_0_pre7/src/L1TriggerDPG/L1Ntuples/test/QCD_GEN_SIM_RAW.root')
-    # fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/QCD_Pt-80to120_Tune4C_13TeV_pythia8/GEN-SIM-RAW/AVE20BX25_tsg_castor_PHYS14_25_V3-v1/00000/00679EAE-098E-E411-9AFC-0025905A6104.root')
-    fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/QCD_Pt-1400to1800_Tune4C_13TeV_pythia8/GEN-SIM-RAW/AVE20BX25_tsg_castor_PHYS14_25_V3-v1/00000/0047F5E2-848E-E411-B993-0025905A6076.root')
-    # fileNames = readFiles
-    )
-
-# readFiles.extend( [
-#   "root://xrootd.unl.edu//store/relval/CMSSW_7_3_0/RelValTTbar_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU50ns_MCRUN2_73_V9_71XGENSIM_FIXGT-v1/00000/52957EA0-2AA2-E411-908B-002618FDA265.root"
-# ])
-
-process.output = cms.OutputModule(
-    "PoolOutputModule",
-    splitLevel = cms.untracked.int32(0),
-    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    outputCommands = cms.untracked.vstring('keep *',
-                                           'keep *_ak5GenJets_*_*',
-                                           'keep *_ak4GenJets_*_*',
-                                           # 'keep *_*_*_L1TEMULATION',
-                                           'keep l1tJetBXVector_*_*_*',
-                                           'keep L1GctJetCands_*_*_*',
-                                           'keep l1extraL1JetParticles_*_*_*'
-                                           ),
-    fileName = cms.untracked.string('SimL1Emulator_Stage1_PP.root'),
-    dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string(''),
-        dataTier = cms.untracked.string('')
-    )
-                                           )
-
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('L1Tree.root')
-    # fileName = cms.string('L1Tree_RelVal_Stage1_newRCT.root')
-    # fileName = cms.string('L1Tree_RelVal_Stage1.root')
+    "l1ExtraTreeProducerGenAk4",
+    "l1ExtraTreeProducerGenAk5",
+    "l1ExtraTreeProducerIntern"
 )
-
-
-# Other statements
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag.connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG')
-# process.GlobalTag.globaltag = cms.string('POSTLS162_V2::All')
-process.GlobalTag.globaltag = cms.string('PHYS14_25_V3::All') # for Phys14 AVE20BX25
-# process.GlobalTag.globaltag = cms.string('MCRUN2_73_V9::All')
 
 ##############################
 # Load up Stage 1 emulator
 ##############################
 process.load('L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff')
-
-# GT
-from L1Trigger.Configuration.SimL1Emulator_cff import simGtDigis
-process.simGtDigis = simGtDigis.clone()
-process.simGtDigis.GmtInputTag = 'simGmtDigis'
-process.simGtDigis.GctInputTag = 'caloStage1LegacyFormatDigis'
-process.simGtDigis.TechnicalTriggersInputTags = cms.VInputTag( )
-
-##############################
-# New RCT calibs
-##############################
-from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
-process.newRCTConfig = cms.ESSource("PoolDBESSource",
-   CondDBSetup,
-   connect = cms.string('frontier://FrontierPrep/CMS_COND_L1T'),
-   DumpStat=cms.untracked.bool(True),
-   toGet = cms.VPSet(
-       cms.PSet(
-           record = cms.string('L1RCTParametersRcd'),
-           tag = cms.string('L1RCTParametersRcd_L1TDevelCollisions_ExtendedScaleFactorsV1')
-       )
-   )
+process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
+process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
+    cms.InputTag('simHcalUnsuppressedDigis'),
+    cms.InputTag('simHcalUnsuppressedDigis')
 )
-process.prefer("newRCTConfig")
 
 ##############################
 # Put normal Stage 1 collections into L1ExtraTree
@@ -118,10 +73,12 @@ process.l1ExtraTreeProducer.metLabel = cms.untracked.InputTag("l1ExtraLayer2:MET
 process.l1ExtraTreeProducer.mhtLabel = cms.untracked.InputTag("l1ExtraLayer2:MHT")
 process.l1ExtraTreeProducer.hfRingsLabel = cms.untracked.InputTag("l1ExtraLayer2")
 
-
 ##############################
 # Do Stage 1 internal jet collection (preGtJets)
 ##############################
+# Turn off any existing stage 1 calibrations
+process.caloStage1Params.jetCalibrationType = cms.string("None")
+
 # Conversion from JetBxCollection to L1JetParticles
 process.preGtJetToL1Jet = cms.EDProducer('PreGtJetToL1Jet',
     preGtJetSource = cms.InputTag("simCaloStage1FinalDigis:preGtJets")
@@ -141,12 +98,14 @@ process.l1ExtraTreeProducerIntern.hfRingsLabel = cms.untracked.InputTag("")
 process.l1ExtraTreeProducerIntern.cenJetLabel = cms.untracked.InputTag("preGtJetToL1Jet:PreGtJets")
 process.l1ExtraTreeProducerIntern.maxL1Extra = cms.uint32(50)
 
-# Turn off any existing stage 1 calibrations
-process.caloStage1Params.jetCalibrationType = cms.string("None")
-
 ##############################
 # Do ak5 GenJets
 ##############################
+# Need to make ak5, not included in GEN-SIM-RAW
+# process.load('RecoJets.Configuration.GenJetParticles_cff')
+# process.load('RecoJets.Configuration.RecoGenJets_cff')
+# process.antiktGenJets = cms.Sequence(process.genJetParticles*process.ak5GenJets)
+
 # Convert ak5 genjets to L1JetParticle objects, store in another L1ExtraTree as cenJets
 process.genJetToL1JetAk5 = cms.EDProducer("GenJetToL1Jet",
     genJetSource = cms.InputTag("ak5GenJets")
@@ -173,7 +132,6 @@ process.l1ExtraTreeProducerGenAk4 = process.l1ExtraTreeProducerIntern.clone()
 process.l1ExtraTreeProducerGenAk4.cenJetLabel = cms.untracked.InputTag("genJetToL1JetAk4:GenJets")
 process.l1ExtraTreeProducerGenAk4.maxL1Extra = cms.uint32(50)
 
-
 ##############################
 # Store PU info (nvtx, etc)
 ##############################
@@ -181,13 +139,25 @@ process.puInfo = cms.EDAnalyzer("PileupInfo",
     pileupInfoSource = cms.InputTag("addPileupInfo")
 )
 
+##############################
+# New RCT calibs
+##############################
+if new_RCT_calibs:
+    print "*** Using new RCT calibs"
+    process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_condDBv2_cff')
+    from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+    # Format: map{(record,label):(tag,connection),...}
+    recordOverrides = { ('L1RCTParametersRcd', None) : ('L1RCTParametersRcd_L1TDevelCollisions_ExtendedScaleFactorsV2', None) }
+    process.GlobalTag = GlobalTag(process.GlobalTag, gt, recordOverrides)
+    file_append += '_newRCTv2'
+else:
+    process.GlobalTag.globaltag = cms.string(gt+'::All')
 
 process.p1 = cms.Path(
     process.L1TCaloStage1_PPFromRaw
-    # +process.simGtDigis
     +process.l1ExtraLayer2
     +process.preGtJetToL1Jet # convert preGtJets into L1Jet objs
-    # +process.antiktGenJets # make ak4GenJets
+    # +process.antiktGenJets # make ak4GenJets - not needed in Phys14
     +process.genJetToL1JetAk5 # convert ak5GenJets to L1Jet objs
     +process.genJetToL1JetAk4 # convert ak4GenJets to L1Jet objs
     +process.l1ExtraTreeProducer # normal Stage 1 stuff in L1ExtraTree
@@ -197,12 +167,68 @@ process.p1 = cms.Path(
     +process.puInfo # store nVtx info
     )
 
-# process.output_step = cms.EndPath(process.output)
 
-process.schedule = cms.Schedule(
-    process.p1
-    # process.output_step
-    )
+##############################
+# Input/output & standard stuff
+##############################
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
+# Input source
+
+# Some default testing files
+if gt == 'PHYS14_25_V3':
+    readFiles = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Phys14DR/QCD_Pt-120to170_Tune4C_13TeV_pythia8/GEN-SIM-RAW/AVE20BX25_tsg_castor_PHYS14_25_V3-v1/00000/004DD38A-2B8E-E411-8E4F-003048FFD76E.root')
+elif gt == 'MCRUN2_74_V6':
+    readFiles = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/RunIISpring15Digi74/QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8/GEN-SIM-RAW/AVE_30_BX_50ns_tsg_MCRUN2_74_V6-v1/00000/00D772EF-41F3-E411-90EF-0025907FD242.root')
+
+process.source = cms.Source("PoolSource",
+                            fileNames = readFiles
+                            )
+
+edm_filename = 'SimStage1Emulator{0}.root'.format(file_append)
+process.output = cms.OutputModule(
+    "PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    # outputCommands = cms.untracked.vstring('keep *'),
+    outputCommands = cms.untracked.vstring(
+        'drop *',
+
+        # Keep TPs
+        'keep *_simHcalTriggerPrimitiveDigis_*_*',
+        'keep *_hcalDigis_*_*',
+        'keep *_ecalDigis_*_*',
+
+        # Keep RCT regions
+        'keep *_simRctDigis_*_*',
+
+        # Keep GenJets
+        'keep *_ak5GenJets_*_*',
+        'keep *_ak4GenJets_*_*',
+
+        'keep *_*_*_L1TEMULATION',
+        'keep l1tJetBXVector_*_*_*',
+        'keep L1GctJetCands_*_*_*',
+        'keep l1extraL1JetParticles_*_*_*'
+    ),
+    fileName = cms.untracked.string(edm_filename)
+)
+
+# output file
+output_filename = 'L1Tree{0}.root'.format(file_append)
+print "*** Writing NTuple to {0}".format(output_filename)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(output_filename)
+)
+
+process.output_step = cms.EndPath(process.output)
+
+process.schedule = cms.Schedule(process.p1)
+
+if save_EDM:
+    print "*** Writing EDM to {0}".format(edm_filename)
+    process.schedule.append(process.output_step)
 
 # Spit out filter efficiency at the end.
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
