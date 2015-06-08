53326b9c4d0ea64c2e85f0019608b82b2c75cb7b
Robin Aggleton <robin.aggleton@cern.ch>
2015-06-08 10:54:25 +0200

    change CMSSW area for matcher_batch, alhtough it should now be updated automatically when running submit_matcher_batch

--- bin/matcher_batch.sh
+++ bin/matcher_batch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # run with bsub -q 1nh "sh matcher_batch.sh <options>"
-# MAKE SURE YOU UPDATE THE AREA, really should automate this
-cd /afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/
+cd /afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/
 RunMatcher $@
