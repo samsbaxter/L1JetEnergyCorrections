d216b80f97f833206229c4b4742125308deb49d5
Robin Aggleton <robin.aggleton@cern.ch>
2015-06-28 22:29:49 +0200

    Change submit_matcher_jobs to take any pairs files, not just those in same directory

--- bin/submit_matcher_jobs.sh
+++ bin/submit_matcher_jobs.sh
@@ -2,26 +2,29 @@
 
 # To submit lots of RunMatcher jobs
 #
-# Add list of L1Tree files to files list
-# Change fdir to point at dir with L1Tree files (pairs output file will go there)
-# Edit the output filename and if applying any corrections
+# Add list of L1Tree files to files list - make sure you use the FULL filepath
 
 declare -a files=(
-L1Tree_QCDFlatPhys14BX50_GCT_QCDFlatPhys14_newRCTv2_calibrated.root
+/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/TTbarSpring15_GCT_oldRCT_oldGCT/L1Tree_TTbarSpring15AVE30BX50_TTbarSpring15_GCT_oldRCT_oldGCT.root
+/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/TTbarSpring15_GCT_newRCTv2_newGCT/L1Tree_TTbarSpring15AVE30BX50_TTbarSpring15_GCT_newRCTv2_newGCT.root
+/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/TTbarSpring15_GCT_newRCTv2_oldGCT/L1Tree_TTbarSpring15AVE30BX50_TTbarSpring15_GCT_newRCTv2_oldGCT.root
 ) 
-
-fdir="/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/QCDFlatPhys14_newRCTv2_calibrated/"
 
 # update the CMSSW area in the batch script
 sed -i s@CMSSW_.*\/src@${CMSSW_VERSION}/src@g matcher_batch.sh
 
 for f in "${files[@]}"
 do
-	jobname=${f#L1Tree_QCD_Pt-}
-	jobname=${jobname%_Phys14_*.root}
-	outname=${f#L1Tree_}
+    fdir=$(dirname $f)
+    filename=$(basename $f)
+	jobname=${filename#L1Tree_}
+	jobname=${jobname%_Spring15_*.root}
+	outname=${filename#L1Tree_}
 	outname=${outname%.root}
 	echo "$outname"
 	echo "$jobname"
-	bsub -q 8nh -J $jobname "sh matcher_batch.sh -I ${fdir}/${f} -O ${fdir}/pairs_${outname}_GCT_ak5_ref14to1000_l10to500.root --l1Dir l1ExtraTreeProducer --l1Branches cenJet FwdJet TauJet"
+    # bsub -q 8nh -J $jobname "sh matcher_batch.sh -I ${fdir}/${f} -O ${fdir}/pairs_${outname}_preGt_ak4_ref14to1000_l10to500.root --refDir l1ExtraTreeProducerGenAk4 --l1Dir l1ExtraTreeProducerIntern"
+    # bsub -q 8nh -J $jobname "sh matcher_batch.sh -I ${fdir}/${f} -O ${fdir}/pairs_${outname}_preGt_ak5_ref14to1000_l10to500.root --refDir l1ExtraTreeProducerGenAk5 --l1Dir l1ExtraTreeProducerIntern"
+    bsub -q 8nh -J $jobname "sh matcher_batch.sh -I ${f} -O ${fdir}/pairs_${outname}_GCT_ak5_ref14to1000_l10to500.root --l1Dir l1ExtraTreeProducer --l1Branches cenJet tauJet fwdJet"
+	bsub -q 8nh -J $jobname "sh matcher_batch.sh -I ${f} -O ${fdir}/pairs_${outname}_GCTintern_ak5_ref14to1000_l10to500.root"
 done
