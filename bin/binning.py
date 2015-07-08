d546c69457177031dc49d63aa35fdccad1e1706c
Robin Aggleton <robin.aggleton@cern.ch>
2015-07-08 11:03:43 +0200

    use numpy import alias for binning.py

--- bin/binning.py
+++ bin/binning.py
@@ -6,26 +6,26 @@ For future, may need to split up into GCT/Stage1 Vs Stage 2...
 
 
 import array
-import numpy
+import numpy as np
 
 
 ############
 # PT BINS
 ############
-pt_bins = list(numpy.arange(14, 254, 4))
+pt_bins = list(np.arange(14, 254, 4))
 # wider binning at higher pt for low stats regions
-pt_bins_wide = list(numpy.concatenate((numpy.arange(14, 50, 4), numpy.arange(50, 270, 20)))) # larger bins at higher pt
+pt_bins_wide = list(np.concatenate((np.arange(14, 50, 4), np.arange(50, 270, 20)))) # larger bins at higher pt
 
 # 8 GeV bins for resolution plots
-pt_bins_8 = list(numpy.arange(14,246,8))
+pt_bins_8 = list(np.arange(14,246,8))
 pt_bins_8.append(250)
 # and wider ones for low stat bins
-pt_bins_8_wide = list(numpy.concatenate((numpy.arange(14,54,8), numpy.arange(54, 242, 20))))
+pt_bins_8_wide = list(np.concatenate((np.arange(14,54,8), np.arange(54, 242, 20))))
 pt_bins_8_wide.append(250)
 
 ############
 # ETA BINS
 ############
-eta_bins = [0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5.001]
+eta_bins = [0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
 eta_bins_central = [eta for eta in eta_bins if eta < 3.1]
 eta_bins_forward = [eta for eta in eta_bins if eta > 2.9]
