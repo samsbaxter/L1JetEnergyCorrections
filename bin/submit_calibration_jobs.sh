c6282098ec2833632bdd0a615de78546a1b7a7d9
Robin Aggleton <robin.aggleton@cern.ch>
2015-07-30 14:35:16 +0100

    Add in ability to loop over multiple pairs files when submitting calibration jobs

--- bin/submit_calibration_jobs.sh
+++ bin/submit_calibration_jobs.sh
@@ -3,9 +3,12 @@
 # To submit lots of runCalibration jobs on lxbatch system (or any bsub)
 # Splits it up by eta bin
 #
-# Change pairs variable to full path of pairs file you want to run over
+# Change pairsFiles to full path of pairs file(s) you want to run over
 # The output files from runCalibration.py will be made in the same directory
 
+declare -a pairsFiles=(
+"/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/QCDSpring15_Stage1_AVE20BX25_newRCTv2/pairs_QCD_Pt-30to1000_Spring15_AVE20BX25_Stage1_QCDSpring15_newRCTv2_preGt_ak5_ref14to1000_l10to500.root"
+)
 
 declare -a etaBins=(
 0
@@ -22,29 +25,36 @@ declare -a etaBins=(
 5
 ) 
 
-pairs="/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/QCDPhys14_newRCTv2/pairs_QCD_Pt-15to600_Phys14_AVE30BX50_GCT_QCDPhys14_newRCTv2_GCT_ak5_ref14to1000_l10to500.root"
+# PU cuts. Optional
+PUmin=-2
+PUmax=120
 
 # update the CMSSW area in the batch script
 sed -i s@CMSSW_.*\/src@${CMSSW_VERSION}/src@g calibration_batch.sh
 
-fdir=`dirname $pairs`
-fname=`basename $pairs`
-
-echo "Using pairs file $pairs"
-echo "Writing output to directory: $fdir"
-
-len=${#etaBins[@]}
-len=$(( len - 1 ))
-for ((i=0;i<$len;++i));
+for pairs in "${pairsFiles[@]}"
 do
-    j=$(( i + 1 ))
-    etamin=${etaBins[i]}
-    etamax=${etaBins[j]}
-    jobname="${etamin}to${etamax}"
-    outname=${fname#pairs_}
-    outname=${outname%.root}
-    outname="output_${outname}_${i}_742.root"
-    echo "$jobname"
-    echo "$outname"
-    bsub -q 8nh -J $jobname "sh calibration_batch.sh --no_genjet_plots ${fdir}/${fname} ${fdir}/${outname} --etaInd ${i}"
+    fdir=`dirname $pairs`
+    fname=`basename $pairs`
+
+    echo "Using pairs file $pairs"
+    echo "Writing output to directory: $fdir"
+
+    len=${#etaBins[@]}
+    len=$(( len - 1 ))
+    for ((i=0;i<$len;++i));
+    do
+        j=$(( i + 1 ))
+        etamin=${etaBins[i]}
+        etamax=${etaBins[j]}
+        jobname="${etamin}to${etamax}"
+        outname=${fname#pairs_}
+        outname=${outname%.root}
+        outname="output_${outname}_${i}.root"
+        # outname="output_${outname}_PU${PUmin}to${PUmax}_${i}.root"  # if you want a filename with PU cuts
+        echo "$jobname"
+        echo "$outname"
+        bsub -q 8nh -J $jobname "sh calibration_batch.sh --no_genjet_plots ${fdir}/${fname} ${fdir}/${outname} --etaInd ${i}"
+        # bsub -q 8nh -J $jobname "sh calibration_batch.sh --no_genjet_plots ${fdir}/${fname} ${fdir}/${outname} --PUmin ${PUmin} --PUmax ${PUmax} --etaInd ${i}"  # if you want a PU cut applied
+    done
 done
