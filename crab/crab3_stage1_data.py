6f1c97273afe4049290a74e6f8f7365c1966a321
Robin Aggleton <robin.aggleton@cern.ch>
2015-08-05 00:25:07 +0200

    Update crab script for stage 1 + data, using proper link to golden JSON, and update python samples file

--- crab/crab3_stage1_data.py
+++ crab/crab3_stage1_data.py
@@ -6,13 +6,13 @@ Run with 'python crab3_stage1.py'
 
 
 from L1Trigger.L1JetEnergyCorrections.crab3_cfg import config
-import L1Trigger.L1JetEnergyCorrections.samples as samples
+import L1Trigger.L1JetEnergyCorrections.data_samples as samples
 from CRABAPI.RawCommand import crabCommand
 import httplib
 
 
 # CHANGE ME - to make a unique indentifier for each set of jobs, e.g v2
-job_append = "Stage1_data_50ns_newLUT_test"
+job_append = "Stage1_data_50ns_oldLUT_all"
 
 # CHANGE ME - select dataset(s) to run over
 datasets = ['Express_Run2015B_50ns']
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         config.Data.splitting = 'LumiBased'
         config.Data.unitsPerJob = dset_opts.unitsPerJob
         # config.Data.lumiMask = "/afs/cern.ch/work/r/raggleto/L1JEC/CMSSW_7_4_6_patch6/src/L1Trigger/L1TCalorimeter/test/Run251718.json"
-        config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt"
+        config.Data.lumiMask = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt"
 
         # to restrict total units run over
         # comment it out to run over all
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         elif 0 < dset_opts.totalUnits < 1:
             config.Data.totalUnits = int(samples.get_number_files(dset_opts.inputDataset) * dset_opts.totalUnits)
         else:
-            config.Data.totalUnits = 1000000000  # make sure we reset
+            config.Data.totalUnits = 1000000000000  # make sure we reset
 
         # print config.Data.totalUnits, "total units"
 
