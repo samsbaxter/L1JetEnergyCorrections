6188a1887d2f0ce4f57ef736c3a7ce81c32ccc80
Robin Aggleton <robin.aggleton@cern.ch>
2015-08-19 14:45:10 +0100

    Add in column auto-sizing for the DAG status monitor script

--- bin/HTCondor/status.py
+++ bin/HTCondor/status.py
@@ -40,6 +40,7 @@ class DagStatus(ClassAd):
         self.nodes_failed = int(nodes_failed)
         self.job_procs_held = int(job_procs_held)
         self.job_procs_idle = int(job_procs_idle)
+        self.nodes_done_percent = round(100. * self.nodes_done / self.nodes_total)
 
 
 class NodeStatus(ClassAd):
@@ -118,44 +119,55 @@ def process(status_filename):
 
 
 def print_table(dag_status, node_statuses, status_end):
-    """Print a pretty-ish table with important info
+    """Print a pretty-ish table with important info"""
+    # Here we auto-create the formatting strings for each row,
+    # and auto-size each column based on max size of contents
 
-    TODO: auto-size the columns. Use terminal window width?
-    """
-    # rows, columns = os.popen('stty size', 'r').read().split()
-    # rows = int(rows)
-    # columns = int(int(columns)*0.75)
-    columns = 80
-
-    # Print info for each job.
+    # For info about each node:
     job_dict = OrderedDict()  # holds column title as key and object attribute name as value
     job_dict["Node"] = "node"
     job_dict["Status"] = "node_status"
     job_dict["Retries"] = "retry_count"
-
     # Auto-size each column
     job_col_widths = [max(max(len(str(x.__dict__[v])) for x in node_statuses), len(k)) for k, v in job_dict.iteritems()]
-
-    print "*" * columns
     # make formatter string to be used for each row, auto calculates number of columns
-    job_format = " | ".join(["{{:<{}}}"]*len(job_dict.keys())).format(*job_col_widths)
-    print job_format.format(*job_dict.keys())
+    job_format = "  |  ".join(["{{:<{}}}"]*len(job_dict.keys())).format(*job_col_widths)
+    job_header = job_format.format(*job_dict.keys())
+
+    # For info about summary of all jobs:
+    summary_dict = OrderedDict()
+    summary_dict["DAG status"] = "dag_status"
+    summary_dict["Total"] = "nodes_total"
+    summary_dict["Queued"] = "nodes_queued"
+    summary_dict["Ready"] = "nodes_ready"
+    summary_dict["Failed"] = "nodes_failed"
+    summary_dict["Done"] = "nodes_done"
+    summary_dict["Done %"] = "nodes_done_percent"
+    summary_col_widths = [max(len(str(dag_status.__dict__[v])), len(k)) for k, v in summary_dict.iteritems()]
+    summary_format = "  |  ".join(["{{:<{}}}"]*len(summary_dict.keys())).format(*summary_col_widths)
+    summary_header = summary_format.format(*summary_dict.keys())
+
+    # Now figure out how many char columns to occupy for the *** and ---
+    columns = max(len(job_header), len(summary_header))
+    term_rows, term_columns = os.popen('stty size', 'r').read().split()
+    term_rows = int(term_rows)
+    term_columns = int(term_columns)
+    if columns > term_columns:
+        columns = term_columns
+
+    # Now actually print the table
+    print "*" * columns
+    # Print info for each job.
+    print job_header
     print "-" * columns
     for n in node_statuses:
-        print job_format.format(*[n.node, n.node_status, n.retry_count])
+        print job_format.format(*[n.__dict__[v] for v in job_dict.values()])
     print "*" * columns
-
     # print summary of all jobs
-    labels = ['DAG status', 'Total', 'Queued', 'Ready', 'Failed', 'Done']
-    print "{:<25} | {:<7} | {:<7} | {:<7} | {:<7} | {:<7}".format(*labels)
+    print summary_header
     print "-" * columns
-    entries = [dag_status.dag_status, dag_status.nodes_total,
-               dag_status.nodes_queued, dag_status.nodes_ready,
-               dag_status.nodes_failed, dag_status.nodes_done,
-               100. * dag_status.nodes_done / dag_status.nodes_total]
-    print "{:<25} | {:<7} | {:<7} | {:<7} | {:<7} | {:<4} ({:4.1f}%)".format(*entries)
+    print summary_format.format(*[dag_status.__dict__[v] for v in summary_dict.values()])
     print "*" * columns
-
     # print time of next update
     print "Status recorded at:", status_end.end_time
     print "Next update:       ", status_end.next_update
