065ceb845ccf0d95d994f27c22ac8ca109bd3f41
Robin Aggleton <robin.aggleton@cern.ch>
2015-09-14 10:26:36 +0100

    Large modifications to cmsRunCondor to make it more CRAB-like.
    Now exports the user's libs, headers, etc to make a 'sandbox' that is setup
    on the worker node. This sorts out havign to setup the correct packages,
    etc on the worker node. Also automatically gets the right SCRAM_ARCH and CMSSW version.
    
    Much better than before!

--- condor/cmsRunCondor.py
+++ condor/cmsRunCondor.py
@@ -7,30 +7,31 @@ Bit rudimentary. See options with:
 
 cmsRunCondor.py --help
 
-Note that it automatically sets the correct process.source, and process.maxEvents
+Note that it automatically sets the correct input files, providing you give it a
+dataset and specify filesPerJob, and totalFiles.
 
 Robin Aggleton 2015, in a rush
 """
 
 
-import sys
-import argparse
 import os
-from time import strftime
-import subprocess
 import re
+import sys
+import json
 import math
 import logging
+import tarfile
+import argparse
+import subprocess
+from time import strftime
 from itertools import izip_longest
-import json
-import shutil
 
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def cmsRunCondor(in_args):
+def cmsRunCondor(in_args=sys.argv[1:]):
     """Creates a condor job description file with the correct arguments,
     and optionally submit it.
 
@@ -48,13 +49,14 @@ def cmsRunCondor(in_args):
                         help="Number of files to run over, per job.",
                         type=int, default=5)
     parser.add_argument("--totalFiles",
-                        help="Total number of files to run over. Default is ALL (-1). "
-                        "A float between 0 and 1 will run that fraction of the whole dataset.",
+                        help="Total number of files to run over. "
+                        "Default is ALL (-1). Also acceptable is a fraction of "
+                        "the whole dataset (0 - 1), or an integer number of files.",
                         type=float, default=-1)
     parser.add_argument("--outputScript",
                         help="Optional: name of condor submission script. "
                         "Default is <config>_<time>.condor")
-    parser.add_argument("--verbose",
+    parser.add_argument("--verbose", "-v",
                         help="Extra printout to clog up your screen.",
                         action='store_true')
     parser.add_argument("--dry",
@@ -65,6 +67,9 @@ def cmsRunCondor(in_args):
                         "This will queue only 1 job, and the job number is instead "
                         "specified by the variable $(index), which should be specified in the DAG script.",
                         action='store_true')
+    parser.add_argument('--log',
+                        help="Location to store job stdout/err/log files. Default is $PWD/jobs.",
+                        default='jobs')
     args = parser.parse_args(args=in_args)
 
     if args.dag:
@@ -75,108 +80,187 @@ def cmsRunCondor(in_args):
 
     log.debug(args)
 
-    # do some checking
+    ###########################################################################
+    # Do some preliminary checking
+    ###########################################################################
     if not os.path.isfile(args.config):
-        log.error("Cannot find config file %s" % args.config)
-        raise IOError
+        err_msg = "Cannot find config file %s" % args.config
+        log.error(err_msg)
+        raise IOError(err_msg)
+
+    # for now, restrict output dir to /hdfs
+    if not args.outputDir.startswith('/hdfs'):
+        log.error('Output directory not on /hdfs')
+        raise RuntimeError('Output directory not on /hdfs')
 
     if not os.path.exists(args.outputDir):
         print "Output directory doesn't exists, trying to make it:", args.outputDir
         try:
             os.makedirs(args.outputDir)
         except OSError as e:
-            log.error("Cannot make output dir %s" % args.outputDir)
+            log.error("Cannot make output dir %s", args.outputDir)
             raise
 
-    if args.filesPerJob > args.totalFiles and args.totalFiles > 1:
-        log.error("You can't have --filesPerJob > --totalFiles!")
+    if args.filesPerJob > args.totalFiles and args.totalFiles >= 1:
+        log.error("You can't have filesPerJob > totalFiles!")
         raise RuntimeError
 
     # make an output directory for log files
-    if not os.path.exists(args.outputDir):
-        os.mkdir('jobs')
+    if not os.path.exists(args.log):
+        os.mkdir(args.log)
 
-    # get the total number of files for this dataset using das_client
-    if args.totalFiles < 1:
-        output_summary = subprocess.check_output(['das_client.py','--query',
-                                                  'summary dataset=%s' % args.dataset,
-                                                  '--format=json'],
-                                                  stderr=subprocess.STDOUT)
-        log.debug(output_summary)
-        summary_dict = json.loads(output_summary)
-        totalFiles = int(summary_dict['data'][0]['summary'][0]['nfiles'])
-        args.totalFiles = totalFiles if args.totalFiles == -1 else args.totalFiles * totalFiles
+    ###########################################################################
+    # Lookup dataset with das_client to determine number of files/jobs
+    ###########################################################################
+    cmds = ['das_client.py',
+            '--query',
+            'summary dataset=%s' % args.dataset,
+            '--format=json']
+    output_summary = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
+    log.debug(output_summary)
+    summary = json.loads(output_summary)
+
+    # check to make sure dataset is valid
+    if summary['status'] == 'fail':
+        log.error('Error querying dataset with das_client:')
+        log.error(summary['reason'])
+        raise RuntimeError('Error querying dataset with das_client')
+
+    # get required number of files
+    # can either have:
+    # < 0 : all files
+    # 0 - 1 : use that fraction of the dataset
+    # >= 1 : use that number of files
+    num_dataset_files = int(summary['data'][0]['summary'][0]['nfiles'])
+    if args.totalFiles < 0:
+        args.totalFiles = num_dataset_files
+    elif args.totalFiles < 1:
+        args.totalFiles *= num_dataset_files
+    elif args.totalFiles > num_dataset_files:
+        log.warning("You specified more files than exist. Using all %d files.",
+                    num_dataset_files)
 
     # Figure out correct number of jobs
     total_num_jobs = int(math.ceil(args.totalFiles / float(args.filesPerJob)))
 
-    # Make a list of files for each job to avoid doing it on worker node side:
-    output_files = subprocess.check_output(['das_client.py','--query',
-                                            'file dataset=%s' % args.dataset,
-                                            '--limit=%d' % args.totalFiles],
-                                            stderr=subprocess.STDOUT)
+    ###########################################################################
+    # Make a list of input files for each job to avoid doing it on worker node
+    ###########################################################################
+    cmds = ['das_client.py',
+            '--query',
+            'file dataset=%s' % args.dataset,
+            '--limit=%d' % args.totalFiles]
+    output_files = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
 
-    list_of_files = ['"%s"' % line for line in output_files.splitlines() if line.lower().startswith("/store")]
+    list_of_files = ['"{0}"'.format(line) for line in output_files.splitlines()
+                     if line.lower().startswith("/store")]
 
     def grouper(iterable, n, fillvalue=None):
+        """
+        Iterate through iterable in groups of size n.
+        If < n values available, pad with fillvalue.
+
+        Taken from the itertools cookbook.
+        """
         args = [iter(iterable)] * n
         return izip_longest(fillvalue=fillvalue, *args)
 
-    fileListName = "fileList%s.py" % args.dataset.replace("/", "_").replace("-", "_")
-    with open(fileListName, "w") as file_list:
+    input_file_list = "fileList%s.py" % args.dataset.replace("/", "_").replace("-", "_")
+    with open(input_file_list, "w") as file_list:
         file_list.write("fileNames = {")
         for n, chunk in enumerate(grouper(list_of_files, args.filesPerJob)):
             file_list.write("%d: [%s],\n" % (n, ', '.join(filter(None, chunk))))
         file_list.write("}")
-    log.info("List of files for each jobs written to %s" % fileListName)
 
+    log.info("List of files for each jobs written to %s", input_file_list)
 
+    ###########################################################################
+    # Make sandbox of user's libs/c++/py files
+    ###########################################################################
+    sandbox_file = "sandbox.tgz"
+    sandbox_dirs = ['biglib', 'lib', 'module', 'python']
+    tar = tarfile.open(sandbox_file, mode="w:gz", dereference=True)
+    cmssw_base = os.environ['CMSSW_BASE']
+    for directory in sandbox_dirs:
+        fullPath = os.path.join(cmssw_base, directory)
+        if os.path.isdir(fullPath):
+            log.debug('Adding %s to tar', fullPath)
+            tar.add(fullPath, directory, recursive=True)
+
+    # special case for /src - need to include src/package/sub_package/data
+    # and src/package/sub_package/interface
+    src_dirs = ['data', 'interface']
+    src_path = os.path.join(cmssw_base, 'src')
+    for root, dirs, files in os.walk(os.path.join(cmssw_base, 'src')):
+        if os.path.basename(root) in src_dirs:
+            d = root.replace(src_path, 'src')
+            log.debug('Adding %s to tar', d)
+            tar.add(root, d, recursive=True)
+
+    # add in the config file and input filelist
+    tar.add(args.config, arcname="config.py")
+    tar.add(input_file_list, arcname="filelist.py")
+
+    # TODO: add in any other files the user wants
+
+    tar.close()
+
+    # copy to /hdfs or /storage to avoid transfer/copying issues
+    sandbox_location = os.path.join(args.outputDir, sandbox_file)
+    if args.outputDir.startswith('/hdfs'):
+        cmd = "hadoop fs -copyFromLocal -f %s %s" % (sandbox_file, args.outputDir.replace("/hdfs", ""))
+        subprocess.call(cmd.split())
+    else:
+        raise Exception("Not a valid output dir for sandbox - not /hdfs")
+
+    # check tar filesize - if it's too big, we won't want to transport it
+    # tar_size = os.path.getsize(sandbox_file) / (1024 * 1024)  # in MB
+    # if tar_size > 100:
+    #     log.warning('%s is %d MB. This can cause issues if submitting many jobs.',
+    #                 sandbox_file, tar_size)
+    #     log.warning('Auto-submit has been disabled so you can check it')
+    #     args.dry = True
+
+    ###########################################################################
     # Make a condor submission script
-    log.debug("Will be submitting %d jobs, running over %d files" % (total_num_jobs, args.totalFiles))
+    ###########################################################################
+    log.debug("Will be submitting %d jobs, running over %d files",
+              total_num_jobs, args.totalFiles)
 
     with open('cmsRun_template.condor') as template:
         job_template = template.read()
 
     config_filename = os.path.basename(args.config)
-    job_filename = '%s_%s.condor' % (config_filename.replace(".py", ""), strftime("%H%M%S"))
-
     if not args.outputScript:
-        args.outputScript = job_filename
+        args.outputScript = '%s_%s.condor' % (config_filename.replace(".py", ""),
+                                              strftime("%H%M%S"))
 
-    job_description = job_template.replace("SEDINITIAL", "")  # keep initialdir local, otherwise tonnes of files on hdfs
+    job = job_template.replace("SEDINITIAL", "")  # don't use initialdir for now
     log_dir = "%s/%s" % (strftime("%d_%b_%y"), args.dataset.split("/")[1])
     log_str = "%s/%s" % (log_dir, args.outputScript.replace(".condor", ""))
     if not os.path.exists("jobs/%s" % log_dir):
         os.makedirs("jobs/%s" % log_dir)
-    job_description = job_description.replace("SEDNAME", log_str)
+    job = job.replace("SEDNAME", log_str)
 
-    args_str = "%s %s %s $(%s)" % (config_filename, fileListName, args.outputDir, "index" if args.dag else "process")
-    job_description = job_description.replace("SEDARGS", args_str)
-    worker_script = 'cmsRun_worker.sh'
-    job_description = job_description.replace("SEDEXE", worker_script)
-    job_description = job_description.replace("SEDNJOBS", str(1) if args.dag else str(total_num_jobs))
-    job_description = job_description.replace("SEDINPUTFILES", "%s, %s" % (os.path.abspath(args.config), fileListName))
+    args_dict = dict(output=args.outputDir,
+                     ind="index" if args.dag else "process",
+                     sandbox=sandbox_location)
+    args_str = "-o {output} -i $({ind}) -a $ENV(SCRAM_ARCH) " \
+               "-c $ENV(CMSSW_VERSION) -S {sandbox}".format(**args_dict)
+    job = job.replace("SEDARGS", args_str)
+    job = job.replace("SEDEXE", 'cmsRun_worker.sh')
+    job = job.replace("SEDNJOBS", str(1) if args.dag else str(total_num_jobs))
+    # transfers = [os.path.abspath(args.config), input_file_list, sandbox_file]
+    transfers = []
+    job = job.replace("SEDINPUTFILES", ", ".join(transfers))
 
     with open(args.outputScript, 'w') as submit_script:
-        submit_script.write(job_description)
-    log.info('New condor submission script written to %s' % args.outputScript)
+        submit_script.write(job)
+    log.info('New condor submission script written to %s', args.outputScript)
 
-    # modify the worker node script with your credentials, and the right CMSSW
-    git_name = subprocess.check_output(['git', 'config', '--global', 'user.github']).strip('\n')
-    git_email = subprocess.check_output(['git', 'config', '--global', 'user.email']).strip('\n')
-    cmssw_version = os.environ['CMSSW_VERSION']
-    # need to make a copy of the file, then rewrite the original. If only I could sed...
-    backup_worker = worker_script+'.bak'
-    shutil.copy(worker_script, backup_worker)
-    with open(worker_script, "w") as worker, open(backup_worker, "r") as orig:
-        for line in orig:
-            line = re.sub(r'git config --global user.github ".*"', 'git config --global user.github "%s"' % git_name, line)
-            line = re.sub(r'git config --global user.email ".*"', 'git config --global user.email "%s"' % git_email, line)
-            line = re.sub(r'cmssw_version=.*', 'cmssw_version=%s' % cmssw_version , line)
-            worker.write(line)
-    os.remove(backup_worker)
-
-    # submit to queue unless dry-run
+    ###########################################################################
+    # submit to queue unless dry run
+    ###########################################################################
     if not args.dry:
         subprocess.call(['condor_submit', args.outputScript])
 
@@ -186,10 +270,10 @@ def cmsRunCondor(in_args):
                 totalNumJobs=total_num_jobs,
                 totaNumFiles=args.totalFiles,
                 filesPerJob=args.filesPerJob,
-                fileList=fileListName,
+                fileList=input_file_list,
                 config=args.config
                 )
 
 
 if __name__ == "__main__":
-    cmsRunCondor(sys.argv[1:])
+    cmsRunCondor()
--- condor/cmsRun_worker.sh
+++ condor/cmsRun_worker.sh
@@ -1,98 +1,151 @@
 #!/bin/bash -e
 
-#################################################
+###############################################################################
 # Script to run cmsRun on condor worker node
-#################################################
+#
+# Briefly, it:
+# - setups up environment & CMSSW
+# - extracts all the user's libs, header files, etc from a sandbox zip
+# - makes a wrapper script for the CMSSW config file,
+# so that it uses the correct input/output files
+# - runs cmsRun
+# - moves the output to /hdfs
+###############################################################################
 
-# Store args
-# TODO: replace with getopts
-script=$1
-filelist=$2
-outputDir=$3
-ind=$4 # ind is the job number
-
-worker="$PWD" # top level of worker node
+worker=$PWD # top level of worker node
 export HOME=$worker # need this if getenv = false
 
-######################
-# First setup CMSSW
-######################
-cmssw_version=CMSSW_7_4_2
+###############################################################################
+# Store args
+###############################################################################
+script="config.py" # cmssw config script filename
+filelist="filelist.py" # py file with dict of input file
+outputDir="" # output directory for result
+ind="" # ind is the job number
+arch="" # architecture
+cmssw_version="" # cmssw version
+sandbox="" # sandbox location
+
+while getopts ":s:f:o:i:a:c:S:" opt; do
+    case $opt in
+        \?)
+            echo "Invalid option $OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+        s)
+            echo "Config filename: $OPTARG"
+            script=$OPTARG
+            ;;
+        f)
+            echo "filelist: $OPTARG"
+            filelist=$OPTARG
+            ;;
+        o)
+            echo "outputDir: $OPTARG"
+            outputDir=$OPTARG
+            ;;
+        i)
+            echo "Index: $OPTARG"
+            ind=$OPTARG
+            ;;
+        a)
+            echo "ARCH: $OPTARG"
+            arch=$OPTARG
+            ;;
+        c)
+            echo "CMSSW: $OPTARG"
+            cmssw_version=$OPTARG
+            ;;
+        S)
+            echo "Sandbox location: $OPTARG"
+            sandbox=$OPTARG
+            ;;
+    esac
+done
+
+###############################################################################
+# TODO: Make a private area for the job incase there are multiple on the same worker?
+###############################################################################
+
+###############################################################################
+# Setup CMSSW
+###############################################################################
+export SCRAM_ARCH=${arch}
 echo "Setting up ${cmssw_version} ..."
 echo "... sourcing CMS default environment from CVMFS"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 echo "... creating CMSSW project area"
 scramv1 project CMSSW ${cmssw_version}
 cd ${cmssw_version}/src
-eval `scramv1 runtime -sh`
+eval `scramv1 runtime -sh`  # cmsenv
 echo "${cmssw_version} has been set up"
 
-# Setup your git config, otherwise cannot check stuff out of git
-git config --global user.github "knobbly"
-git config --global user.email "knobbly@email"
-git config -l
-export CMSSW_GIT_REFERENCE="${worker}/.cmsgit-cache"
+###############################################################################
+# Extract sandbox of user's libs, headers, and python files
+###############################################################################
+cd ..
+cp $sandbox sandbox.tgz
+tar xvzf sandbox.tgz
 
-######################
-# Setup packages
-# Do whatever you need to make sure all necessary packages are checked out, etc
-# git cms-addpkg doesn't work for now :(
-######################
-# git cms-addpkg L1Trigger/L1TCalorimeter
-git clone https://github.com/cms-l1-dpg/L1Ntuples.git L1TriggerDPG/L1Ntuples
-git clone https://github.com/raggleton/L1JetEnergyCorrections.git L1Trigger/L1JetEnergyCorrections
-# Get Golden JSON
-# wget https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt
-
-######################
+###############################################################################
 # Make a wrapper config
 # This will setup the input files, output file, and number of events
 # Means that the user doesn't have to do anything special to their config file
-######################
-pyfile="wrapper.py"
+###############################################################################
+wrapper="wrapper.py"
 
-echo "import FWCore.ParameterSet.Config as cms" >> $pyfile
-echo "import "${script%.py}" as myscript" >> $pyfile
-echo "import ${filelist%.py} as filelist" >> $pyfile
+echo "import FWCore.ParameterSet.Config as cms" >> $wrapper
+echo "import "${script%.py}" as myscript" >> $wrapper
+echo "import ${filelist%.py} as filelist" >> $wrapper
+echo "process = myscript.process" >> $wrapper
+echo "process.source.fileNames = cms.untracked.vstring(filelist.fileNames[$ind])" >> $wrapper
+echo "process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))" >> $wrapper
+echo "if hasattr(process, 'TFileService'): process.TFileService.fileName = "\
+"cms.string(process.TFileService.fileName.value().replace('.root', '_${ind}.root'))" >> $wrapper
+echo "process.output.fileName = cms.untracked.string("\
+"process.output.fileName.value().replace('.root', '_${ind}.root'))" >> $wrapper
 
-# Write more overriding things to wrapper
-echo "process = myscript.process" >> $pyfile
-echo "process.source.fileNames = cms.untracked.vstring(filelist.fileNames[$ind])" >> $pyfile
-echo "process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))" >> $pyfile
-echo "process.TFileService.fileName = cms.string(process.TFileService.fileName.value().replace('.root', '_${ind}.root'))" >> $pyfile
-echo "process.output.fileName = cms.string(process.output.fileName.value().replace('.root', '_${ind}.root'))" >> $pyfile
-
+echo "==== Wrapper script ====="
 echo ""
-echo "Wrapper script:"
+cat $wrapper
 echo ""
-cat $pyfile
+echo "========================="
+
+###############################################################################
+# Log the modified script
+###############################################################################
+
+echo "=== CMS config script ==="
 echo ""
+cat $script
+echo ""
+echo "========================="
 
-
-######################
-# Move the cms config script * list of files from top level
-# to CMSSW area and build everything
-######################
-mv $worker/$script .
-mv $worker/fileList* .
-scram b
-ls
-
-######################
+###############################################################################
 # Now finally run script!
 # TODO: some automated retry system
-######################
-cmsRun $pyfile
+###############################################################################
+cmsRun $wrapper
 echo "CMS JOB OUTPUT" $?
 
 ls -l
 
-######################
-# Copy across to hdfs. Don't need /hdfs bit when using hadoop tools
-# TODO: add in check as to whether the user wants it on hdfs or not...
-######################
+###############################################################################
+# Copy across to output directory.
+# Check if hdfs or not.
+# Don't need /hdfs bit when using hadoop tools
+###############################################################################
 for f in $(find . -name "*.root" -maxdepth 1)
 do
     output=$(basename $f)
-    hadoop fs -copyFromLocal -f $output ${outputDir///hdfs}/$output
+    echo "Copying $output to $outputDir"
+    if [[ "$outputDir" == /hdfs* ]]; then
+        hadoop fs -copyFromLocal -f $output ${outputDir///hdfs}/$output
+    elif [[ "$outputDir" == /storage* ]]; then
+        cp $output $outputDir
+    fi
 done
