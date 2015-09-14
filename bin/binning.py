db527f840e65dca4e29d91df9686bfeaabb33fae
Robin Aggleton <robin.aggleton@cern.ch>
2015-09-14 10:27:49 +0100

    Add in set of 11 colours to binning.py so can be used to distigush eta bins in scripts.

--- bin/binning.py
+++ bin/binning.py
@@ -5,8 +5,11 @@ For future, may need to split up into GCT/Stage1 Vs Stage 2...
 """
 
 
+import ROOT
 import array
 import numpy as np
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 
 ############
@@ -30,3 +33,5 @@ eta_bins = [0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
 eta_bins_all = [-5, -4.5, -4.0, -3.5, -3.0, -2.172, -1.74, -1.392, -1.044, -0.695, -0.348, 0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
 eta_bins_central = [eta for eta in eta_bins if eta < 3.1]
 eta_bins_forward = [eta for eta in eta_bins if eta > 2.9]
+# a handy palette of colours
+eta_bin_colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kBlack, ROOT.kMagenta, ROOT.kOrange+7, ROOT.kAzure+1, ROOT.kRed+3, ROOT.kViolet+1, ROOT.kOrange, ROOT.kTeal-5]
