3402f1a0d22d428b3da2468bbf49c11605dff7eb
Robin Aggleton <robin.aggleton@cern.ch>
2015-09-17 14:19:22 +0100

    Add lots more plots to puInvestigations.py, and do in various pt bins. Also add in option to plot 0PU sample alongside PU plots.

--- bin/smallStudies/puInvestigations.py
+++ bin/smallStudies/puInvestigations.py
@@ -4,10 +4,11 @@ Little script to showoff plots that may showup PU regions Vs signal regions
 
 import argparse
 import ROOT
-from common_utils import *
 import os
 import sys
-import binning
+from itertools import izip, product, chain
+import L1Trigger.L1JetEnergyCorrections.analysis.common_utils as cu
+import L1Trigger.L1JetEnergyCorrections.analysis.binning as binning
 
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
@@ -18,12 +19,13 @@ ROOT.gStyle.SetOptStat(0);
 ROOT.gStyle.SetPalette(55)
 
 
-def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
-    f = open_root_file(input_filename)
-    tree = get_from_file(f, "valid")
+def plot_deltaR(input_file, out_dir, refPtMin=0, internJet=False, append=""):
+    """Plot deltaR distributions for various pT, eta bins"""
 
-    c = ROOT.TCanvas("c", "", 800, 600)
-    c.SetLogz()
+    tree = cu.get_from_file(input_file, "valid")
+
+    canv = ROOT.TCanvas("canv", "", 800, 600)
+    canv.SetLogz()
 
     colours = [ROOT.kBlack, ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2]
 
@@ -48,7 +50,7 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
                 tree.Draw("dr>>%s" % (hname), total_cut)
                 h_dr_pt.Scale(1./h_dr_pt.Integral())
                 out_name = hname+".pdf"
-                c.SaveAs(out_dir+"/"+out_name)
+                canv.SaveAs(out_dir+"/"+out_name)
 
                 h_dr_pt.SetLineColor(colours[i])
                 mg.Add(h_dr_pt)
@@ -58,12 +60,10 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
             mg.GetHistogram().SetTitle("%s < |#eta^{L1}| < %g;#Delta R(L1, GenJet);" % (eta_min, eta_max))
             leg.Draw()
             out_name = mg.GetName()+".pdf"
-            c.SaveAs(out_dir+"/"+out_name)
+            canv.SaveAs(out_dir+"/"+out_name)
 
 
     # DeltaR vs response for given pT bin
-
-    f.Close()
 
     # Draw DeltaR Vs Delta pT
     # if internJet:
@@ -76,7 +76,7 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
     # out_name += "refMin%g" % refPtMin
     # out_name += append
     # out_name += ".pdf"
-    # c.SaveAs(out_dir+"/"+out_name)
+    # canv.SaveAs(out_dir+"/"+out_name)
 
     # Draw pt (l1) Vs Delta pT
     # if internJet:
@@ -89,7 +89,7 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
     # out_name += "refMin%g" % refPtMin
     # out_name += append
     # out_name += ".pdf"
-    # c.SaveAs(out_dir+"/"+out_name)
+    # canv.SaveAs(out_dir+"/"+out_name)
 
     # Draw pt (gen) Vs Delta pT
     # if internJet:
@@ -102,10 +102,10 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
     # out_name += "refMin%g" % refPtMin
     # out_name += append
     # out_name += ".pdf"
-    # c.SaveAs(out_dir+"/"+out_name)
+    # canv.SaveAs(out_dir+"/"+out_name)
 
     # Draw deltaEta vs deltaPhi
-    # c.SetLogz(0)
+    # canv.SetLogz(0)
     # h_deta_dphi = ROOT.TH2D("h_deta_dphi", "", 70, -0.7, 0.7, 70, -0.7, 0.7)
     # if internJet:
     #     h_deta_dphi.SetTitle("Intern jets, p_{T}^{Gen} > %g GeV;#Delta#phi;#Delta#eta" % refPtMin)
@@ -117,17 +117,183 @@ def make_plots(input_filename, out_dir, refPtMin=0, internJet=False, append=""):
     # out_name += "refMin%g" % refPtMin
     # out_name += append
     # out_name += ".pdf"
-    # c.SaveAs(out_dir+"/"+out_name)
+    # canv.SaveAs(out_dir+"/"+out_name)
 
 
-def main(in_args=sys.argv[1:]):
+def plot_response_nvtx(input_file, out_dir, nvtx_var="numPUVertices",
+              eta_min=0, eta_max=5, pt_min=0, pt_max=250, pt_var="pt", append=""):
+    """Plot response as a function of number of vertices
+
+    nvtx_var is the variable that describes number of vertices, is normally
+    either numPUVertices or trueNumInteractions.
+
+    eta_min/max, pt_min/max are for cuts on TMath::Abs(eta) and pt.
+    pt_var is the variable name for the pt cut, so you can change the variable to cut on
+    """
+    tree = cu.get_from_file(input_file, "valid")
+
+    canv = ROOT.TCanvas("canv", "", 800, 600)
+    canv.SetTicks(1, 1)
+
+    # check nvtx_var in tree
+    if not hasattr(tree, nvtx_var):
+        raise RuntimeError("%s is not a valid branch name" % nvtx_var)
+
+    # cuts
+    eta_cut = "%f < TMath::Abs(eta) && TMath::Abs(eta) < %f" % (eta_min, eta_max)
+    pt_cut = "%f < %s && %s < %f" % (pt_min, pt_var, pt_var, pt_max)
+    cuts = [eta_cut, pt_cut]
+    total_cut = " && ".join(cuts)
+    print total_cut
+
+    # simple 2D plot of response vs nVtx
+    hname = "h_nvtx_rsp_eta_%g_%g_pt_%g_%g" % (eta_min, eta_max, pt_min, pt_max)
+    h_nvtx_rsp = ROOT.TH2D(hname,
+                        "%g < |#eta| < %g, %g < %s < %g;%s;response ( = L1/Ref)" % (eta_min, eta_max, pt_min, pt_var, pt_max, nvtx_var),
+                        40, 0, 40, 60, 0, 3)
+    tree.Draw("rsp:%s>>%s" % (nvtx_var, hname), total_cut, "COLZ")
+
+    # now permutate over log/norm
+    for norm, log in product([True, False], [True, False]):
+        canv.Clear()
+        append_new = append
+        if log:
+            canv.SetLogz(1)
+            append_new += "_log"
+        else:
+            canv.SetLogz(0)
+
+        if norm:
+            append_new += "_norm"
+            hnew = cu.norm_vertical_bins(h_nvtx_rsp)
+            hnew.Draw("COLZ")
+        else:
+            h_nvtx_rsp.Draw("COLZ")
+
+        canv.SaveAs(out_dir+"/%s%s.pdf" % (hname, append_new))
+
+
+def plot_response_nvtx_binned(input_file, out_dir, no_pu_file, nvtx_var="numPUVertices",
+                              eta_min=0, eta_max=5, pt_min=0, pt_max=250, pt_var="pt", append=""):
+    """Make histogram of response for various bins of nVtx
+
+    no_pu_file is optional TFile for 0PU sample.
+
+    eta_min/max, pt_min/max are for cuts on TMath::Abs(eta) and pt.
+
+    pt_var is the variable name for the pt cut, so you can change the variable to cut on.
+
+    nvtx_var is the variable that describes number of vertices, is normally
+    either numPUVertices or trueNumInteractions.
+
+    """
+
+    tree = cu.get_from_file(input_file, "valid")
+    tree_no_pu = cu.get_from_file(no_pu_file, "valid") if no_pu_file else None
+
+    canv = ROOT.TCanvas("canv", "", 800, 600)
+    canv.SetTicks(1, 1)
+
+    # check nvtx_var in tree
+    if not hasattr(tree, nvtx_var):
+        raise RuntimeError("%s is not a valid branch name" % nvtx_var)
+
+    # cuts
+    eta_cut = "%f < TMath::Abs(eta) && TMath::Abs(eta) < %f" % (eta_min, eta_max)
+    pt_cut = "%f < %s && %s < %f" % (pt_min, pt_var, pt_var, pt_max)
+    cuts = [eta_cut, pt_cut]
+
+    stack = ROOT.THStack("st", "")
+    nbins, rsp_min, rsp_max = 40, 0, 2
+    lw = 2
+
+    leg = ROOT.TLegend(0.56, 0.6, 0.87, 0.87)
+
+    # make hist if 0PU sample available
+    if no_pu_file:
+        hname = "h_rsp_eta_%g_%g_%s_%g_%g_no_pu" % (eta_min, eta_max, pt_var, pt_min, pt_max)
+        h = ROOT.TH1D(hname,
+                      "%g < |#eta| < %g, %g < %s < %g;response (=L1/ref);p.d.f." % (eta_min, eta_max, pt_min, pt_var, pt_max),
+                      nbins, rsp_min, rsp_max)
+        total_cut = " && ".join(cuts)
+        print total_cut
+        tree_no_pu.Draw("rsp>>%s" % hname, total_cut)
+        h.Scale(1. / h.Integral())
+        h.SetLineColor(ROOT.kBlack)
+        h.SetLineWidth(lw)
+        h.SetMarkerColor(ROOT.kBlack)
+        leg.AddEntry(h, "nVtx: 0 (0PU sample)", "L")
+        stack.Add(h)
+
+    # make hist for each nVtx bin
+    nvtx_bins = ([0, 10], [15, 25], [30, 40])
+    colours = [ROOT.kRed, ROOT.kGreen+2, ROOT.kBlue]
+    for i, (nvtx_min, nvtx_max) in enumerate(nvtx_bins):
+        hname = "h_rsp_eta_%g_%g_%s_%g_%g_nvtx_%g_%g" % (eta_min, eta_max, pt_var, pt_min, pt_max, nvtx_min, nvtx_max)
+        h = ROOT.TH1D(hname,
+                      "%g < |#eta| < %g, %g < %s < %g;response (=L1/ref);p.d.f." % (eta_min, eta_max, pt_min, pt_var, pt_max),
+                      nbins, rsp_min, rsp_max)
+        nvtx_cut = "%f < %s && %s < %f" % (nvtx_min, nvtx_var, nvtx_var, nvtx_max)
+        total_cut = " && ".join(chain(cuts, [nvtx_cut]))
+        print total_cut
+        tree.Draw("rsp>>%s" % hname, total_cut)
+        h.Scale(1. / h.Integral())
+        h.SetLineColor(colours[i])
+        h.SetLineWidth(lw)
+        h.SetMarkerColor(colours[i])
+        leg.AddEntry(h, "nVtx: %g - %g" % (nvtx_min, nvtx_max), "L")
+        stack.Add(h)
+
+    canv.Clear()
+    stack.Draw("NOSTACK HISTE")
+    stack.GetHistogram().SetTitle("%g < |#eta| < %g, %g < %s < %g;response (=L1/ref);p.d.f." % (eta_min, eta_max, pt_min, pt_var, pt_max))
+    stack.GetHistogram().GetYaxis().SetTitleOffset(1.1)
+    leg.Draw()
+    canv.SaveAs(out_dir+"/h_rsp_eta_%g_%g_%s_%g_%g%s.pdf" % (eta_min, eta_max, pt_var, pt_min, pt_max, append))
+
+
+def pu_investigate(in_args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--pairs", help="input ROOT file with matched pairs from RunMatcher")
     parser.add_argument("--oDir", help="Directory to save plots. Default is $PWD.", default=".")
+    parser.add_argument("--noPU", help="Input ROOT file with 0 PU.", default=None)
     args = parser.parse_args(args=in_args)
     print args
-    make_plots(input_filename=args.pairs, out_dir=args.oDir)
+
+    cu.check_dir_exists_create(args.oDir)
+
+    input_file = cu.open_root_file(args.pairs)
+    no_pu_file = cu.open_root_file(args.noPU) if args.noPU else None
+
+    # plot_deltaR(input_file=args.pairs, out_dir=args.oDir)
+
+    # central, forward, for select pt bins
+    for pt_min, pt_max in ([0, 250], [0, 50], [75, 125]):
+        plot_response_nvtx(input_file=input_file, out_dir=args.oDir,
+                    nvtx_var="numPUVertices", eta_min=0, eta_max=3,
+                    pt_min=pt_min, pt_max=pt_max, append="")
+        plot_response_nvtx(input_file=input_file, out_dir=args.oDir,
+                    nvtx_var="numPUVertices", eta_min=3, eta_max=5,
+                    pt_min=pt_min, pt_max=pt_max, append="")
+
+        plot_response_nvtx_binned(input_file=input_file, no_pu_file=no_pu_file,
+                                out_dir=args.oDir,
+                                nvtx_var="numPUVertices", eta_min=0, eta_max=3,
+                                pt_min=pt_min, pt_max=pt_max, append="")
+        plot_response_nvtx_binned(input_file=input_file, no_pu_file=no_pu_file,
+                                out_dir=args.oDir,
+                                nvtx_var="numPUVertices", eta_min=3, eta_max=5,
+                                pt_min=pt_min, pt_max=pt_max, append="")
+
+    # Individual eta bins
+    # for eta_min, eta_max in zip(binning.eta_bins[:-1], binning.eta_bins[1:]):
+    #     print eta_min, eta_max
+    #     plot_response_nvtx(input_file=input_file, out_dir=args.oDir, nvtx_var="numPUVertices",
+    #         eta_min=eta_min, eta_max=eta_max, pt_min=0, pt_max=250, append="")
+
+
+    input_file.Close()
 
 
 if __name__ == "__main__":
-    main()
+    pu_investigate()
