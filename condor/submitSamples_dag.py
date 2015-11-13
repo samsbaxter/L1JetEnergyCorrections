3a7ea67e89c4f7edcf036ee90e884d28227ce999
Robin Aggleton <robin.aggleton@cern.ch>
2015-11-13 01:18:25 +0000

    Update submitSamples_dag to reflect new capabilities of cmsRunCondor

--- condor/submitSamples_dag.py
+++ condor/submitSamples_dag.py
@@ -20,16 +20,10 @@ from time import strftime, sleep
 from subprocess import call
 
 
-config = "../python/SimL1Emulator_Stage1_newRCT.py"
-outputDir = "/hdfs/user/ra12451/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/Stage1_QCDFlatSpring15BX25HCALFix_stage1v2_rctv4_jetSeed5"
+config = "../python/SimL1Emulator_Stage2_newLut.py"
+outputDir = "/hdfs/user/ra12451/L1JEC/CMSSW_7_5_0_pre5/L1JetEnergyCorrections/Stage2_QCDFlatSpring15BX25HCALFix_layer2NewLut11NovPU15to25_jetSeed0"
 
-# config = "../python/SimL1Emulator_Stage1_newRCT_noStage1Lut.py"
-# outputDir = "/hdfs/user/ra12451/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/Stage1_QCDFlatSpring15BX25HCALFix_stage1NoLut_rctv4_jetSeed5"
 datasets = ['QCDFlatSpring15BX25PU10to30HCALFix', 'QCDFlatSpring15BX25FlatNoPUHCALFix']
-
-# config = "../python/SimL1Emulator_Stage1_newRCT_noStage1Lut_jetSeed5_noPUS.py"
-# outputDir = "/hdfs/user/ra12451/L1JEC/CMSSW_7_4_2/src/L1Trigger/L1JetEnergyCorrections/Stage1_QCDFlatSpring15BX25HCALFix_stage1NoLut_rctv4_jetSeed5_noPUS"
-# datasets = ['QCDFlatSpring15BX25FlatNoPUHCALFix']
 
 if __name__ == "__main__":
     # Run through datasets once to check all fine
@@ -56,32 +50,5 @@ if __name__ == "__main__":
                                  '--filesPerJob', str(dset_opts.unitsPerJob),
                                  '--totalFiles', str(dset_opts.totalUnits),
                                  '--outputScript', scriptName,
-                                 '--dry',
-                                 '--dag', # important!
-                                 # '--verbose'
+                                 '--dag'
                                  ])
-
-        # Setup DAG file for this dataset
-        dag_name = "jobs_%s_%s.dag" % (dset, strftime("%d_%b_%y_%H%M%S"))
-        print "DAG Name:", dag_name
-        with open(dag_name, "w") as dag_file:
-            dag_file.write("# DAG for dataset %s\n" % dset_opts.inputDataset)
-            dag_file.write("# Using config file %s\n" % config)
-            # dag_file.write("# Writing to dir %s\n" % str(outputDir+"/"+dset))
-            for job_ind in xrange(job_dict['totalNumJobs']):
-                jobName = "%s_%d" % (dset, job_ind)
-                dag_file.write('JOB %s %s\n' % (jobName, scriptName))
-                dag_file.write('VARS %s index="%d"\n' % (jobName, job_ind))
-                dag_file.write('RETRY %s 3\n' % jobName)
-            status_file = dag_name.replace(".dag", ".status")
-            status_names.append(status_file)
-            dag_file.write("NODE_STATUS_FILE %s 30\n" % status_file)
-
-        # Submit DAG
-        call(['condor_submit_dag', dag_name])
-
-        print "Check DAG status:"
-        print "./DAGstatus.py", status_names[-1]
-
-    print "To view the status of all DAGs:"
-    print "./DAGstatus.py", " ".join(status_names)
