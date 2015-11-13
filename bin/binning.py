6cd43706f945bba9a7387275196626dff38361b0
Robin Aggleton <robin.aggleton@cern.ch>
2015-11-13 01:07:38 +0000

    Add in bins in binning.py for doing checkCalibration in pt bins, and improve spacing

--- bin/binning.py
+++ bin/binning.py
@@ -6,8 +6,8 @@ For future, may need to split up into GCT/Stage1 Vs Stage 2...
 
 
 import ROOT
-import array
 import numpy as np
+
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
@@ -35,12 +35,26 @@ pt_bins_8_wide = list(np.concatenate((np.arange(14, 54, 8),
                                       np.arange(54, 242, 20))))
 pt_bins_8_wide.append(250)
 
+# pt bins for doing checks
+check_pt_bins = [[0, 20], [20, 40], [40, 60], [60, 80], [80, 120], [120, 200], [200, 300], [300, 500], [500, 1000]]
+
 ############
 # ETA BINS
 ############
 eta_bins = [0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
-eta_bins_all = [-5, -4.5, -4.0, -3.5, -3.0, -2.172, -1.74, -1.392, -1.044, -0.695, -0.348, 0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
+eta_bins_all = [-5, -4.5, -4.0, -3.5, -3.0, -2.172, -1.74, -1.392, -1.044, -0.695, -0.348,
+                0.0, 0.348, 0.695, 1.044, 1.392, 1.74, 2.172, 3.0, 3.5, 4.0, 4.5, 5]
 eta_bins_central = [eta for eta in eta_bins if eta < 3.1]
 eta_bins_forward = [eta for eta in eta_bins if eta > 2.9]
 # a handy palette of colours
-eta_bin_colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kBlack, ROOT.kMagenta, ROOT.kOrange+7, ROOT.kAzure+1, ROOT.kRed+3, ROOT.kViolet+1, ROOT.kOrange, ROOT.kTeal-5]
+eta_bin_colors = [ROOT.kRed,
+                  ROOT.kBlue,
+                  ROOT.kGreen + 2,
+                  ROOT.kBlack,
+                  ROOT.kMagenta,
+                  ROOT.kOrange + 7,
+                  ROOT.kAzure + 1,
+                  ROOT.kRed + 3,
+                  ROOT.kViolet + 1,
+                  ROOT.kOrange,
+                  ROOT.kTeal - 5]
