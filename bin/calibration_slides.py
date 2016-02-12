f9555573c44d21f8f2c536323c232feb5acae569
Robin Aggleton <robin.aggleton@cern.ch>
2016-02-12 13:29:03 +0000

    Fix calibration_slides.py to work with output files that are missing the fit graphs.

--- bin/calibration_slides.py
+++ bin/calibration_slides.py
@@ -24,7 +24,8 @@ ROOT.gROOT.SetBatch(1)
 etaBins = binning.eta_bins
 
 
-def plot_to_file(f, plotname, filename, xtitle="", ytitle="", title="", drawopts="", drawfit=True, extend_fit=True):
+def plot_to_file(f, plotname, filename, xtitle="", ytitle="", title="",
+                 drawopts="", drawfit=True, extend_fit=True):
     """
     f is a TFile
     filename is output, can be a list of filenames (e.g. for .tex, .pdf, .png)
@@ -33,54 +34,60 @@ def plot_to_file(f, plotname, filename, xtitle="", ytitle="", title="", drawopts
     ROOT.gStyle.SetPaperSize(10., 10.)
     ROOT.gStyle.SetOptStat("mre")
     ROOT.gStyle.SetOptFit(0101)  # MAGIC COMBO FOR PARAMS+ERRORS
-    # p = ROOT.TGraphErrors(f.Get(plotname).Clone())
     print plotname
     obj = f.Get(plotname)
     obj2 = f.Get(plotname + "_fit")
-    if not obj or (drawfit and not obj2):
+    if not obj:
         print "Can't find", obj
         return False
-    else:
-        p = obj.Clone()
-        p2 = obj2.Clone()
-        p.GetYaxis().SetTitle(ytitle)
-        p2.GetYaxis().SetTitle(ytitle)
-        p.GetXaxis().SetTitle(xtitle)
-        p2.GetXaxis().SetTitle(xtitle)
-        p.SetTitle(title)
-        p2.SetTitle(title)
-        if not drawfit:
-            p2.GetListOfFunctions().Remove(p.GetListOfFunctions().At(0))
-        else:
-            fn = p2.GetListOfFunctions().At(0)
-            if fn:
-                fn.SetLineWidth(2)
-        p.Draw(drawopts)
-        p2.Draw(drawopts + "SAME")
-        # draw fit over full range, not just fitted range
-        if drawfit and extend_fit:
-            fn = p2.GetListOfFunctions().At(0)
-            if fn:
-                fn2 = fn.Clone()
-                fn2.SetRange(0, 1000)
-                fn2.SetLineStyle(3)
-                fn2.SetLineWidth(2)
-                fn2.Draw("SAME")
-        p.Draw(drawopts + "SAME")
-        ROOT.gPad.Update()
-        ROOT.gPad.SetTicks(1, 1)
 
-        # Draw fit stats box
+    if (drawfit and not obj2):
+        print "Can't find", plotname + "_fit, not drawing fit function"
+        drawfit = False
+
+    p = obj.Clone()
+    # p.GetYaxis().SetTitle(ytitle)
+    # p.GetXaxis().SetTitle(xtitle)
+    p.SetTitle(';'.join([title, xtitle, ytitle]))
+    p.Draw(drawopts)
+
+    if drawfit:
+        p2 = obj2.Clone()
+        # p2.GetYaxis().SetTitle(ytitle)
+        # p2.GetXaxis().SetTitle(xtitle)
+        p2.SetTitle(';'.join([title, xtitle, ytitle]))
+        fn = p2.GetListOfFunctions().At(0)
+        if fn:
+            fn.SetLineWidth(2)
+        p2.Draw(drawopts + "SAME")
+
+        # draw fit over full range, not just fitted range
+        if extend_fit:
+            fn2 = fn.Clone()
+            fn2.SetRange(0, 1000)
+            fn2.SetLineStyle(3)
+            fn2.SetLineWidth(2)
+            fn2.Draw("SAME")
+        p.Draw(drawopts + "SAME")
+
+    ROOT.gPad.Update()
+    ROOT.gPad.SetTicks(1, 1)
+
+    # Draw fit stats box
+    if drawfit:
         st = p2.FindObject("stats")
         st.SetFillStyle(0)
         st.SetX1NDC(0.6)
         st.SetX2NDC(0.9)
         st.SetY1NDC(0.55)
         st.SetY2NDC(0.9)
-        for f in filename:
-            ROOT.gPad.Print(f)
-        ROOT.gPad.Clear()
-        return True
+
+    for f in filename:
+        ROOT.gPad.Print(f)
+
+    ROOT.gPad.Clear()
+
+    return True
 
 
 def plot_corr_results(in_name=""):
