6830f747c47332d626fc444b622b7220d591c204
Robin Aggleton <robin.aggleton@cern.ch>
2016-02-16 22:35:25 +0000

    Move all L1JEC config code to a customisation module & functions, to allow easy reproducibility.

--- python/SimL1Emulator_Stage2_HF_MC.py
+++ python/SimL1Emulator_Stage2_HF_MC.py
@@ -10,7 +10,7 @@ Stores L1 jets, as well as ak4 GenJets
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: l1Ntuple -s RAW2DIGI --era=Run2_2016 --customise=L1Trigger/Configuration/customise_Stage2Calo.Stage2CaloFromRaw --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --conditions=auto:run2_mc -n 10 --mc --filein=/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root --geometry=Extended2016,Extended2016Reco
+# with command line options: l1Ntuple -s RAW2DIGI --era=Run2_2016 --customise=L1Trigger/Configuration/customise_Stage2Calo.Stage2CaloFromRaw --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --customise=L1Trigger/L1JetEnergyCorrections/customiseL1JEC.L1NtupleJEC_OFF --conditions=auto:run2_mc -n 10 --mc --filein=/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root --geometry=Extended2016,Extended2016Reco --no_output
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
@@ -23,7 +23,7 @@ from Configuration.StandardSequences.Eras import eras
 save_EDM = False
 
 # Things to append to L1Ntuple/EDM filename (globalTag added later)
-file_append = "_Stage2_Spring15MC_HF_10Feb_"
+file_append = "_Stage2_Spring15MC_HF_16Feb_noJec"
 
 #############################################################################
 
@@ -39,7 +39,7 @@ process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 200
 process.MessageLogger.suppressWarning = cms.untracked.vstring(
@@ -51,12 +51,17 @@ process.MessageLogger.suppressWarning = cms.untracked.vstring(
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
 
 # Input source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root'),
+    fileNames = cms.untracked.vstring(
+        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root',
+        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root',
+        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/00EA9A04-CD4E-E511-8F7B-001517E7410C.root',
+        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/026B6D9B-C24E-E511-A5A1-0025905938A4.root'
+        ),
     # fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/002DE782-FFA9-E511-9BBB-0025905964A6.root'),
     secondaryFileNames = cms.untracked.vstring()
 )
@@ -90,7 +95,6 @@ process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
-# process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_v12')
 file_append += '_' + process.GlobalTag.globaltag.value()
 
 # Path and EndPath definitions
@@ -117,72 +121,16 @@ from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleEMU
 # call to customisation function L1NtupleRAWEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
 process = L1NtupleEMU(process)
 
+# Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1JEC
+from L1Trigger.L1JetEnergyCorrections.customiseL1JEC import L1NtupleJEC_OFF
+
+# call to customisation function L1NtupleJEC imported from L1Trigger.L1JetEnergyCorrections.customiseL1JEC
+process = L1NtupleJEC_OFF(process)
+
 # End of customisation functions
-
-# Get rid of unnecessary Stage 1 modules
-remove_modules = [
-    process.L1TRawToDigi,
-    process.siPixelDigis,
-    process.siStripDigis,
-    process.muonCSCDigis,
-    process.muonDTDigis,
-    process.muonRPCDigis,
-    process.castorDigis
-]
-for mod in remove_modules:
-    result = process.RawToDigi.remove(mod)
-    if not result:
-        raise RuntimeError('Could not remove %s' % mod.label())
-
-# Add L1Tree
-process.load('L1Trigger.L1TNtuples.l1Tree_cfi')
-
-# Do ak4 GenJets
-# Convert ak4 genjets to L1JetParticle objects
-process.genJetToL1JetAk4 = cms.EDProducer("GenJetToL1Jet",
-    genJetSource = cms.InputTag("ak4GenJets")
-)
-
-# Put in another L1ExtraTree as cenJets
-process.load("L1Trigger.L1TNtuples.l1ExtraTree_cfi")
-process.l1ExtraTreeGenAk4 = process.l1ExtraTree.clone()
-process.l1ExtraTreeGenAk4.nonIsoEmToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.isoEmToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.tauJetToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.isoTauJetToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.cenJetToken = cms.untracked.InputTag("genJetToL1JetAk4:GenJets")
-process.l1ExtraTreeGenAk4.fwdJetToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.muonToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.metToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.mhtToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.hfRingsToken = cms.untracked.InputTag("")
-process.l1ExtraTreeGenAk4.maxL1Extra = cms.uint32(50)
-
-# Store PU info (nvtx, etc)
-process.puInfo = cms.EDAnalyzer("PileupInfo",
-    pileupInfoSource = cms.InputTag("addPileupInfo")
-)
-
-# Add both GenJets and PU to process path
-add_modules = [
-    process.genJetToL1JetAk4,
-    process.l1ExtraTreeGenAk4,
-    process.puInfo,
-    process.l1Tree
-]
-
-for mod in add_modules:
-    process.l1ntuplesim.insert(len(process.l1ntuplesim.moduleNames()), mod)
 
 # Set the right ECAL input for TPs
 process.l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag("ecalDigis", "EcalTriggerPrimitives")
-
-# Use MP jets
-process.l1UpgradeEmuTree.jetToken = cms.untracked.InputTag("simCaloStage2Digis", "MP")
-
-# Turn off L1JEC
-process.caloStage2Params.jetCalibrationType = cms.string("None")
-file_append += "_noJec"
 
 # Set the NTuple filename
 process.TFileService.fileName = cms.string("L1Ntuple%s.root" % file_append)
---
+++ python/customiseL1JEC.py
@@ -0,0 +1,107 @@
+import FWCore.ParameterSet.Config as cms
+
+
+"""
+Customisation for MC L1NTuples, for generating ntuples suitable for L1JEC.
+
+Only needed for MC!
+
+For **no** L1JEC, i.e. if deriving calibrations, add to cmsDriver.py:
+
+--customise=L1Trigger/L1JetEnergyCorrections/customiseL1JEC.L1NtupleJEC_OFF
+
+To include L1JEC, i.e. if testing/validating calibrations, add to cmsDriver.py:
+
+--customise=L1Trigger/L1JetEnergyCorrections/customiseL1JEC.L1NtupleJEC_ON
+"""
+
+
+def L1NtupleJEC_ON(process):
+    """Setup L1Ntuples for L1JEC, with L1JEC applied."""
+    L1NtupleJEC(process)
+    L1JEC_on(process)
+    return process
+
+
+def L1NtupleJEC_OFF(process):
+    """Setup L1Ntuples for L1JEC, with no L1JEC applied."""
+    L1NtupleJEC(process)
+    L1JEC_off(process)
+    return process
+
+
+def L1NtupleJEC(process):
+    """Customise L1Ntuple for L1JEC for MC.
+
+    Adds in:
+    - GenJets (into the cenJet branch of an L1ExtraTree)
+    - PU Info
+
+    Also removes other modules we don't care about (muons, tracks).
+    """
+    # Convert GenJet obj to format for L1Extra
+    # Such a nasty hack.
+    process.genJetToL1JetAk4 = cms.EDProducer("GenJetToL1Jet",
+        genJetSource = cms.InputTag("ak4GenJets")
+    )
+
+    # Put in another L1ExtraTree as cenJets
+    process.load("L1Trigger.L1TNtuples.l1ExtraTree_cfi")
+    process.l1ExtraTreeGenAk4 = process.l1ExtraTree.clone()
+    process.l1ExtraTreeGenAk4.nonIsoEmToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.isoEmToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.tauJetToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.isoTauJetToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.cenJetToken = cms.untracked.InputTag("genJetToL1JetAk4:GenJets")
+    process.l1ExtraTreeGenAk4.fwdJetToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.muonToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.metToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.mhtToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.hfRingsToken = cms.untracked.InputTag("")
+    process.l1ExtraTreeGenAk4.maxL1Extra = cms.uint32(50)
+
+    # Store PU info (nvtx, etc)
+    process.puInfo = cms.EDAnalyzer("PileupInfo",
+        pileupInfoSource = cms.InputTag("addPileupInfo")
+    )
+
+    # Add it all to the schedule as new Path
+    process.l1jec = cms.Path(
+        process.genJetToL1JetAk4 +
+        process.l1ExtraTreeGenAk4 +
+        process.puInfo)
+
+    process.schedule.append(process.l1jec)
+
+    # Use MP jets for JEC
+    process.l1UpgradeEmuTree.jetToken = cms.untracked.InputTag("simCaloStage2Digis", "MP")
+
+    # Get rid of unnecessary Stage 1 modules
+    remove_modules = [
+        process.L1TRawToDigi,
+        process.siPixelDigis,
+        process.siStripDigis,
+        process.muonCSCDigis,
+        process.muonDTDigis,
+        process.muonRPCDigis,
+        process.castorDigis
+    ]
+    for mod in remove_modules:
+        if mod in process.__dict__.keys():
+            result = process.RawToDigi.remove(mod)
+            if not result:
+                raise RuntimeError('Could not remove %s' % mod.label())
+
+    return process
+
+
+def L1JEC_off(process):
+    """Turn Stage 2 JEC OFF"""
+    process.caloStage2Params.jetCalibrationType = cms.string("None")
+    return process
+
+
+def L1JEC_on(process):
+    """Turn Stage 2 JEC ON"""
+    process.caloStage2Params.jetCalibrationType = cms.string("function6PtParams22EtaBins")
+    return process
