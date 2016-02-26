5586172c87161a5d5294d3145ec56e893ee8095b
Robin Aggleton <robin.aggleton@cern.ch>
2016-02-26 18:15:53 +0000

    Update Stage 2 MC config to work with new integration branch, and allow for easy modification of jet seed threshold

--- python/SimL1Emulator_Stage2_HF_MC.py
+++ python/SimL1Emulator_Stage2_HF_MC.py
@@ -5,29 +5,16 @@ Now comes with HF!
 
 Stores L1 jets, as well as ak4 GenJets
 """
-
 # Auto generated configuration file
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: l1Ntuple -s RAW2DIGI --era=Run2_2016 --customise=L1Trigger/Configuration/customise_Stage2Calo.Stage2CaloFromRaw --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --customise=L1Trigger/L1JetEnergyCorrections/customiseL1JEC.L1NtupleJEC_OFF --conditions=auto:run2_mc -n 10 --mc --filein=/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root --geometry=Extended2016,Extended2016Reco --no_output
+# with command line options: l1Ntuple -s RAW2DIGI --era=Run2_2016 --conditions=auto:run2_mc -n 100 --mc --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs --geometry=Extended2016,Extended2016Reco --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleAODEMU --customise=L1Trigger/L1JetEnergyCorrections/customiseL1JEC.L1NtupleJEC_OFF --no_output --python_filename=SimL1Emulator_Stage2_HF_Layer1_MC_test.py --no_exec --customise=L1Trigger/Configuration/customiseUtils.L1TTurnOffGtAndGmtEmulation --customise_commands=process.l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag('ecalDigis', 'EcalTriggerPrimitives')\nprocess.l1CustomReco.replace(process.ak4PFCHSL1FastL2L3ResidualCorrectorChain, process.ak4PFCHSL1FastL2L3CorrectorChain)\nprocess.l1JetRecoTree.jecToken = 'ak4PFCHSL1FastL2L3Corrector' --filein=/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/929E688D-E94E-E511-8AD2-0026189438EA.root,/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/C04FFC53-F14E-E511-91BA-002590593876.root,/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/EAB9AC8A-E94E-E511-8B45-002618943935.root --secondfilein=/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
 
-#############################################################################
-# COMMON FLAGS:
-#
-# To save the EDM content as well:
-# (WARNING: DON'T do this for big production - will be HUGE)
-save_EDM = False
-
-# Things to append to L1Ntuple/EDM filename (globalTag added later)
-file_append = "_Stage2_Spring15MC_HF_20Feb_noL1Jec_V7PFJEC"
-
-#############################################################################
-
-process = cms.Process('L1Reco',eras.Run2_2016)
+process = cms.Process('RAW2DIGI',eras.Run2_2016)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -37,18 +24,9 @@ process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 200
-process.MessageLogger.suppressWarning = cms.untracked.vstring(
-    "l1UpgradeEmuTree",
-    # "l1CaloTowerTree",
-    # "gtStage2Digis",
-    # "gmtStage2Digis",
-    'l1ExtraTreeGenAk4'
-)
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
@@ -56,90 +34,82 @@ process.maxEvents = cms.untracked.PSet(
 
 # Input source
 process.source = cms.Source("PoolSource",
-    # fileNames = cms.untracked.vstring(
-    secondaryFileNames = cms.untracked.vstring(
-        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root',
-        # 'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root',
-        # 'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/00EA9A04-CD4E-E511-8F7B-001517E7410C.root',
-        # 'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/026B6D9B-C24E-E511-A5A1-0025905938A4.root'
-        ),
-    # fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/RunIIFall15DR76/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/25nsFlat0to50NzshcalRaw_76X_mcRun2_asymptotic_v12-v1/20000/002DE782-FFA9-E511-9BBB-0025905964A6.root'),
-    fileNames = cms.untracked.vstring(
-        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/929E688D-E94E-E511-8AD2-0026189438EA.root',
-        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/C04FFC53-F14E-E511-91BA-002590593876.root',
-        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/EAB9AC8A-E94E-E511-8B45-002618943935.root'
-        )
+    fileNames = cms.untracked.vstring('/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/929E688D-E94E-E511-8AD2-0026189438EA.root'),
+        # '/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/C04FFC53-F14E-E511-91BA-002590593876.root',
+        # '/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RECO/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/EAB9AC8A-E94E-E511-8B45-002618943935.root'),
+    secondaryFileNames = cms.untracked.vstring('/store/mc/RunIISpring15DR74/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/GEN-SIM-RAW/NhcalZSHFscaleFlat10to30Asympt25ns_MCRUN2_74_V9-v1/00000/003BEF9B-C24E-E511-B4B7-0025905A609E.root')
 )
 
 process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
+
+)
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 200
+process.MessageLogger.suppressWarning = cms.untracked.vstring(
+    "l1UpgradeEmuTree",
+    'l1ExtraTreeGenAk4'
 )
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('l1Ntuple nevts:10'),
+    annotation = cms.untracked.string('l1Ntuple nevts:100'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
 
 # Output definition
 
-process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string(''),
-        filterName = cms.untracked.string('')
-    ),
-    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    fileName = cms.untracked.string('l1Ntuple_RAW2DIGI_%s.root' % file_append),
-    outputCommands = process.RECOSIMEventContent.outputCommands,
-    splitLevel = cms.untracked.int32(0),
-)
-
 # Additional output definition
 
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
-file_append += '_' + process.GlobalTag.globaltag.value()
 
 # Path and EndPath definitions
 process.raw2digi_step = cms.Path(process.RawToDigi)
 process.endjob_step = cms.EndPath(process.endOfProcess)
-process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
 
 # Schedule definition
 process.schedule = cms.Schedule(process.raw2digi_step,process.endjob_step)
-if save_EDM:
-    process.schedule.append(process.RECOSIMoutput_step)
 
 # customisation of the process.
 
-# Automatic addition of the customisation function from L1Trigger.Configuration.customise_Stage2Calo
-from L1Trigger.Configuration.customise_Stage2Calo import Stage2CaloFromRaw 
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseReEmul
+from L1Trigger.Configuration.customiseReEmul import L1TReEmulFromRAW,L1TEventSetupForHF1x1TPs 
 
-# call to customisation function Stage2CaloFromRaw imported from L1Trigger.Configuration.customise_Stage2Calo
-process = Stage2CaloFromRaw(process)
+#call to customisation function L1TReEmulFromRAW imported from L1Trigger.Configuration.customiseReEmul
+process = L1TReEmulFromRAW(process)
+
+#call to customisation function L1TEventSetupForHF1x1TPs imported from L1Trigger.Configuration.customiseReEmul
+process = L1TEventSetupForHF1x1TPs(process)
 
 # Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1Ntuple
-from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleAODEMU
+from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleAODEMU 
 
-# call to customisation function L1NtupleRAWEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
+#call to customisation function L1NtupleAODEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
 process = L1NtupleAODEMU(process)
 
-# Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1JEC
-from L1Trigger.L1JetEnergyCorrections.customiseL1JEC import L1NtupleJEC_OFF
+# Automatic addition of the customisation function from L1Trigger.L1JetEnergyCorrections.customiseL1JEC
+from L1Trigger.L1JetEnergyCorrections.customiseL1JEC import L1NtupleJEC_OFF 
 
-# call to customisation function L1NtupleJEC imported from L1Trigger.L1JetEnergyCorrections.customiseL1JEC
+#call to customisation function L1NtupleJEC_OFF imported from L1Trigger.L1JetEnergyCorrections.customiseL1JEC
 process = L1NtupleJEC_OFF(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseUtils
+from L1Trigger.Configuration.customiseUtils import L1TTurnOffGtAndGmtEmulation 
+
+#call to customisation function L1TTurnOffGtAndGmtEmulation imported from L1Trigger.Configuration.customiseUtils
+process = L1TTurnOffGtAndGmtEmulation(process)
 
 # End of customisation functions
 
-# Set right RECO JEC for MC
+# Customisation from command line
+process.l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag('ecalDigis', 'EcalTriggerPrimitives')
 process.l1CustomReco.replace(process.ak4PFCHSL1FastL2L3ResidualCorrectorChain, process.ak4PFCHSL1FastL2L3CorrectorChain)
 process.l1JetRecoTree.jecToken = 'ak4PFCHSL1FastL2L3Corrector'
 
-# Set the right ECAL input for TPs
-process.l1CaloTowerEmuTree.ecalToken = cms.untracked.InputTag("ecalDigis", "EcalTriggerPrimitives")
-
+# Modify the jet seed threshold, default was 1.5
+jet_seed_threshold = 3
+process.caloStage2Params.jetSeedThreshold = cms.double(jet_seed_threshold)
 # Set the NTuple filename
-process.TFileService.fileName = cms.string("L1Ntuple%s.root" % file_append)
+process.TFileService.fileName = cms.string("L1Ntuple_Stage2_Spring15MC_HF_26Feb_noL1Jec_%s_jst%s.root" % (process.GlobalTag.globaltag.value(), str(jet_seed_threshold).replace('.', 'p')))
