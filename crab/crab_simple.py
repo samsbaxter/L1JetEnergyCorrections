591292e28eb1932cb933dfbc851d40e75819cab7
Robin Aggleton <robin.aggleton@cern.ch>
2016-03-11 09:07:24 +0000

    Update crab simple with data-specific parts

--- crab/crab_simple.py
+++ crab/crab_simple.py
@@ -1,5 +1,5 @@
 """
-CRAB3 simple config.
+CRAB3 simple config, setup for MC. For DATA, uncomment the relevant section below
 
 Blank strings should be filled in!
 
@@ -16,20 +16,21 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = ""  # the cmssw config file path
 config.JobType.inputFiles = ['Fall15_25nsV2_MC.db']
 
-config.Data.splitting = 'FileBased'
-# config.Data.splitting = 'LumiBased'  # use this for data
-
 config.Data.publication = False
 
 config.Data.inputDataset = ""  # your AOD/RECO dataset here
 config.Data.useParent = True  # for 2-file solution
-config.Data.unitsPerJob = 1
 
-# These values only make sense for processing data:
-# Select input data based on a lumi mask
-# config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
-# Select input data based on run-ranges
-# config.Data.runRange = '260627'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1  # play around with this
+
+# For processing DATA:
+# -------------------------------------------------
+# config.JobType.inputFiles = ['Fall15_25nsV2_DATA.db']
+# config.Data.splitting = 'LumiBased'  # use this for data
+# config.Data.unitsPerJob = 4
+# config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt'
+# config.Data.runRange = '260627'  # Select input data based on run-ranges
 
 # Where the output files will be transmitted to
 config.Site.storageSite = 'T2_UK_SGrid_Bristol'
