e4af9c9548a68aa99571d2c8888efcd38c50c791
Robin Aggleton <robin.aggleton@cern.ch>
2016-03-16 18:31:46 +0000

    Update cmsRunCondor to work with a user-defined filelist instead of auto-crawling a dataset

--- condor/cmsRunCondor.py
+++ condor/cmsRunCondor.py
@@ -31,6 +31,33 @@ logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
+def generate_filelist_filename(dataset):
+    dset_uscore = dataset[1:]
+    dset_uscore = dset_uscore.replace("/", "_").replace("-", "_")
+    return "fileList_%s.py" % dset_uscore
+
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Iterate through iterable in groups of size n.
+    If < n values available, pad with fillvalue.
+
+    Taken from the itertools cookbook.
+    """
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+
+
+def create_filelist(list_of_files, files_per_job, filelist_filename):
+    with open(filelist_filename, "w") as file_list:
+        file_list.write("fileNames = {")
+        for n, chunk in enumerate(grouper(list_of_files, files_per_job)):
+            file_list.write("%d: [%s],\n" % (n, ', '.join(filter(None, chunk))))
+        file_list.write("}")
+
+    log.info("List of files for each jobs written to %s", filelist_filename)
+
+
 def cmsRunCondor(in_args=sys.argv[1:]):
     """Creates a condor job description file with the correct arguments,
     and optionally submit it.
@@ -53,6 +80,9 @@ def cmsRunCondor(in_args=sys.argv[1:]):
                         "Default is ALL (-1). Also acceptable is a fraction of "
                         "the whole dataset (0-1), or an integer number of files.",
                         type=float, default=-1)
+    parser.add_argument("--filelist",
+                        help="Pass in a list of filenames to run over. "
+                        "This will ignore --dataset option.")
     parser.add_argument("--outputScript",
                         help="Optional: name of condor submission script. "
                         "Default is <config>_<time>.condor")
@@ -95,6 +125,11 @@ def cmsRunCondor(in_args=sys.argv[1:]):
         log.error(err_msg)
         raise IOError(err_msg)
 
+    if args.filelist:
+        args.filelist = os.path.abspath(args.filelist)
+        if not os.path.isfile(args.filelist):
+            raise IOError("Cannot find filelist %s" % args.filelist)
+
     # for now, restrict output dir to /hdfs
     if not args.outputDir:
         raise RuntimeError('You must specify an output directory')
@@ -126,80 +161,68 @@ def cmsRunCondor(in_args=sys.argv[1:]):
     if args.profile:
         # placehold vars
         total_num_jobs = 1
-        dset_uscore = 'profiling'
         input_file_list = None
 
     else:
-        if not args.dataset:
-            raise RuntimeError('You must specify a dataset')
+        if not args.filelist and not args.dataset:
+            raise RuntimeError('You must specify a dataset or a filelist')
 
-        # TODO: use das_client API
-        log.info("Querying DAS for dataset info, please be patient...")
-        cmds = ['das_client.py',
-                '--query',
-                'summary dataset=%s' % args.dataset,
-                '--format=json']
-        output_summary = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
-        log.debug(output_summary)
-        summary = json.loads(output_summary)
+        if not args.filelist:
+            # TODO: use das_client API
+            log.info("Querying DAS for dataset info, please be patient...")
+            cmds = ['das_client.py',
+                    '--query',
+                    'summary dataset=%s' % args.dataset,
+                    '--format=json']
+            output_summary = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
+            log.debug(output_summary)
+            summary = json.loads(output_summary)
 
-        # check to make sure dataset is valid
-        if summary['status'] == 'fail':
-            log.error('Error querying dataset with das_client:')
-            log.error(summary['reason'])
-            raise RuntimeError('Error querying dataset with das_client')
-        # If profiling, use whatver files defined in the config script
+            # check to make sure dataset is valid
+            if summary['status'] == 'fail':
+                log.error('Error querying dataset with das_client:')
+                log.error(summary['reason'])
+                raise RuntimeError('Error querying dataset with das_client')
 
-        # get required number of files
-        # can either have:
-        # < 0 : all files
-        # 0 - 1 : use that fraction of the dataset
-        # >= 1 : use that number of files
-        num_dataset_files = int(summary['data'][0]['summary'][0]['nfiles'])
-        if args.totalFiles < 0:
-            args.totalFiles = num_dataset_files
-        elif args.totalFiles < 1:
-            args.totalFiles = math.ceil(args.totalFiles * num_dataset_files)
-        elif args.totalFiles > num_dataset_files:
-            log.warning("You specified more files than exist. Using all %d files.",
-                        num_dataset_files)
+            # get required number of files
+            # can either have:
+            # < 0 : all files
+            # 0 - 1 : use that fraction of the dataset
+            # >= 1 : use that number of files
+            num_dataset_files = int(summary['data'][0]['summary'][0]['nfiles'])
+            if args.totalFiles < 0:
+                args.totalFiles = num_dataset_files
+            elif args.totalFiles < 1:
+                args.totalFiles = math.ceil(args.totalFiles * num_dataset_files)
+            elif args.totalFiles > num_dataset_files:
+                log.warning("You specified more files than exist. Using all %d files.",
+                            num_dataset_files)
 
-        # Figure out correct number of jobs
-        total_num_jobs = int(math.ceil(args.totalFiles / float(args.filesPerJob)))
+            # Figure out correct number of jobs
+            total_num_jobs = int(math.ceil(args.totalFiles / float(args.filesPerJob)))
 
-        ###########################################################################
-        # Make a list of input files for each job to avoid doing it on worker node
-        ###########################################################################
-        log.info("Querying DAS for filenames, please be patient...")
-        cmds = ['das_client.py',
-                '--query',
-                'file dataset=%s' % args.dataset,
-                '--limit=%d' % args.totalFiles]
-        output_files = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
+            ###########################################################################
+            # Make a list of input files for each job to avoid doing it on worker node
+            ###########################################################################
+            log.info("Querying DAS for filenames, please be patient...")
+            cmds = ['das_client.py',
+                    '--query',
+                    'file dataset=%s' % args.dataset,
+                    '--limit=%d' % args.totalFiles]
+            output_files = subprocess.check_output(cmds, stderr=subprocess.STDOUT)
 
-        list_of_files = ['"{0}"'.format(line) for line in output_files.splitlines()
-                         if line.lower().startswith("/store")]
-
-        def grouper(iterable, n, fillvalue=None):
-            """
-            Iterate through iterable in groups of size n.
-            If < n values available, pad with fillvalue.
-
-            Taken from the itertools cookbook.
-            """
-            args = [iter(iterable)] * n
-            return izip_longest(fillvalue=fillvalue, *args)
-
-        dset_uscore = args.dataset[1:]
-        dset_uscore = dset_uscore.replace("/", "_").replace("-", "_")
-        input_file_list = "fileList_%s.py" % dset_uscore
-        with open(input_file_list, "w") as file_list:
-            file_list.write("fileNames = {")
-            for n, chunk in enumerate(grouper(list_of_files, args.filesPerJob)):
-                file_list.write("%d: [%s],\n" % (n, ', '.join(filter(None, chunk))))
-            file_list.write("}")
-
-        log.info("List of files for each jobs written to %s", input_file_list)
+            list_of_files = ['"{0}"'.format(line) for line in output_files.splitlines()
+                             if line.lower().startswith("/store")]
+            input_file_list = generate_filelist_filename(args.dataset[1:])
+            create_filelist(list_of_files, args.filesPerJob, input_file_list)
+        else:
+            # Get files from user's file
+            with open(args.filelist) as flist:
+                list_of_files = ['"{0}"'.format(line.strip()) for line in flist
+                                 if line.lower().startswith("/store")]
+            total_num_jobs = int(math.ceil(len(list_of_files) / float(args.filesPerJob)))
+            input_file_list = "filelist_user.py"
+            create_filelist(list_of_files, args.filesPerJob, input_file_list)
 
     ###########################################################################
     # Make sandbox of user's libs/c++/py files
@@ -292,7 +315,6 @@ def cmsRunCondor(in_args=sys.argv[1:]):
     # Setup DAG file if needed
     ###########################################################################
     if args.dag:
-        # dag_name = "jobs_%s_%s_%s.dag" % (dset_uscore, date, time)
         args.dag = os.path.realpath(args.dag)
         dag_name = args.dag
         if not os.path.isdir(os.path.dirname(dag_name)):
@@ -300,8 +322,15 @@ def cmsRunCondor(in_args=sys.argv[1:]):
         status_file = dag_name.replace(".dag", ".status")
         log.info("DAG Filename: %s", dag_name)
         with open(dag_name, "w") as dag_file:
-            dag_file.write("# DAG for dataset %s\n" % args.dataset)
             dag_file.write("# Using config file %s\n" % args.config)
+
+            if args.filelist:
+                dset_uscore = os.path.splitext(os.path.basename(args.filelist))[0][:20]
+            elif args.profile:
+                dset_uscore = "profiling"
+            else:
+                dset_uscore = args.dataset[1:].replace("/", "_").replace("-", "_")
+
             for job_ind in xrange(total_num_jobs):
                 jobName = "%s_%d" % (dset_uscore, job_ind)
                 dag_file.write('JOB %s %s\n' % (jobName, args.outputScript))
