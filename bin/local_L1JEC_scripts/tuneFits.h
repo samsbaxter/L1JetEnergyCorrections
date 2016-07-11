0f5bdf6f351cabd4f31f3accd36d377e0fb998f4
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-07-11 19:07:07 +0100

    added objects for easy fitting

--- bin/local_L1JEC_scripts/tuneFits.h
+++ bin/local_L1JEC_scripts/tuneFits.h
@@ -102,7 +102,7 @@ void tuneFits::save()
 	// set the ranges to the fit range(ish)
 	graph->GetXaxis()->SetRangeUser(0,xmax_rec); // will use the y-axis of how you were looking at it
 	// Not as simple for a function
-	string fitName = "fitfcneta_" + etaRangeLabel +"TESTING";
+	string fitName = "fitfcneta_" + etaRangeLabel;
 	TF1 * saveNewFit = new TF1(fitName.c_str(), "[0]+[1]*TMath::Erf([2]*(log10(x)-[3])+[4]*exp([5]*(log10(x)-[6])*(log10(x)-[6])))", xmin_rec, xmax_rec);
 	saveNewFit->SetParameter(0, newFit->GetParameter(0));
 	saveNewFit->SetParameter(1, newFit->GetParameter(1));
@@ -113,13 +113,14 @@ void tuneFits::save()
 	saveNewFit->SetParameter(6, newFit->GetParameter(6));
 
 	string graphNameUse = "l1corr_eta_" + etaRangeLabel + "_fit";
-	// file->Delete(graphNameUse.c_str());
-	// file->Delete(fitName.c_str());
+	file->Delete(graphNameUse.c_str());
+	file->Delete(fitName.c_str());
 	graph->Write();
 	saveNewFit->Write();
 	file->Close();
 }
 
+// remember to use these objects to help you
 // IMPORT THESE OBJECTS FOR EASY INTERACTIVE RUNNING
-string etaNamesDummy[3] = {"0_0.174", "0.174_0.348", "2.65_3.139"};
-tuneFits t0, t1, t2; 
+string etaNames[16] = {"0_0.435", "0.435_0.783", "0.783_1.131", "1.131_1.305", "1.305_1.479", "1.479_1.653", "1.653_1.83", "1.83_1.93", "1.93_2.043", "2.043_2.172", "2.172_2.322", "2.322_2.5", "2.5_2.964", "2.964_3.489", "3.489_4.191", "4.191_5.191"}; 
+tuneFits t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15;
