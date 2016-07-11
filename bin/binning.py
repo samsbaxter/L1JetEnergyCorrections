5b80b1fbee94f636e2636363fe722dd33679bb09
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-07-11 19:06:33 +0100

    higher PU bin added

--- bin/binning.py
+++ bin/binning.py
@@ -206,5 +206,8 @@ eta_bin_colors = [ROOT.kRed,
 ##########
 # PU BINS
 ##########
-pu_bins = [[0, 10], [15, 25], [30, 40]]  # usual binning used for MC studies
+# pu_bins = [[0, 10], [15, 25], [30, 40]]  # usual binning used for MC studies
+pu_bins = [[0, 10], [15, 25], [30, 40], [45, 55]] # a new high-PU bin is added
+# pu_bins = [[45, 55]] # just to do it by itself
+
 pu_bins_lower = [[0, 5], [8, 12], [15, 25]] # run260627 lower PU overall - mean 10
