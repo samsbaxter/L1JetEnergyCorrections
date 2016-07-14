ee36ed9e12da3af4f4d20e8438c6350ff1195524
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-07-14 15:44:27 +0100

    2 copies of fits files

--- bin/local_L1JEC_scripts/runCalibFitsLocally.py
+++ bin/local_L1JEC_scripts/runCalibFitsLocally.py
@@ -7,13 +7,15 @@ import sys
 # and the output file names (either a test or an append)
 # $ python /users/jt15104/CMSSW_8_0_9/src/L1Trigger/L1JetEnergyCorrections/bin/local_L1JEC_scripts/runCalibFitsLocally.py
 
+# may need to re-run if you screw things up whilst trying to massage fits!!!
+
 # note: eta_bins in bin/binning.py must correspond to the type of data you are using!!!
 # if you change CMSSW its name comes into the path ~line 48
 ############################################
 ############################################
 # user defined section #####################
 ############################################
-motherDirectory = "30June2016_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_809v70_noJEC_893ca_etaBinsAllTT/"
+motherDirectory = "30June2016_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_809v70_noJEC_893ca_etaBinsSel16/"
 
 # must ensure that the runCalibration file is set-up to use the same fit! TODO: automate it?
 # daughterDirectory = "runCalib_conventionalFit/"
@@ -21,19 +23,21 @@ motherDirectory = "30June2016_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_809
 daughterDirectory = "runCalib_jetMetFitErr/"
 
 inputFiles = [
-				"output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsAllTT_PU0to10.root",
-				"output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsAllTT_PU15to25.root",
-				"output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsAllTT_PU30to40.root",
+				# "output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsSel16_PU0to10.root",
+				# "output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsSel16_PU15to25.root",
+				# "output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsSel16_PU30to40.root",
+				"output_QCDFlatFall15PU0to50NzshcalRaw_ak4_ref10to5000_l10to5000_dr0p25_etaBinsSel16_PU45to55.root",
 				]
 
 # initially output a test file
 outputFiles = [
-				"testPU0to10.root",
-				"testPU15to25.root",
-				"testPU30to40.root",
+				# "fitsPU0to10.root",
+				# "fitsPU15to25.root",
+				# "fitsPU30to40.root",
+				"fitsPU45to55.root",
 				]
 
-# if you are sure that you wish to append the output onto the input
+# if you are sure that you wish to append the output onto the input (maybe will never pursue this workflow)
 # outputFiles = inputFiles
 ############################################
 ############################################
@@ -42,12 +46,17 @@ if len(inputFiles) != len(outputFiles):
 	print "number of inputs doesn't equal number of outputs...exiting"
 	sys.exit()
 
+outputFiles_initialCopy = []	
+
 for i in range(0, len(inputFiles)):
 	inputFiles[i] = "/users/jt15104/local_L1JEC_store/" + motherDirectory + daughterDirectory + inputFiles[i]
 	outputFiles[i] = "/users/jt15104/local_L1JEC_store/" + motherDirectory + daughterDirectory + outputFiles[i]
 	os.system("python /users/jt15104/CMSSW_8_0_9/src/L1Trigger/L1JetEnergyCorrections/bin/runCalibration.py "
 			+ inputFiles[i] + " " + outputFiles[i]
 			+ " --redo-correction-fit --inherit-params --stage2")
+	outputFiles_initialCopy[i] = outputFiles[i][:-5] + "_initialCopy.root"
+	os.system("cp /users/jt15104/local_L1JEC_store/" + motherDirectory + daughterDirectory + outputFiles[i] + " "
+				"/users/jt15104/local_L1JEC_store/" + motherDirectory + daughterDirectory + outputFiles_initialCopy[i])
 	print "done " + str(i+1) + " of " + str(len(inputFiles)) + " routines"
 
 print "all done:)"
