d728972213126f5cd5c6642d2e91851ecb694500
Joseph Taylor <joseph.ross.taylor@cern.ch>
2017-05-24 13:25:21 +0100

    updated the class so that it is now screw up proof

--- bin/local_L1JEC_scripts/tuneFits.h
+++ bin/local_L1JEC_scripts/tuneFits.h
@@ -12,69 +12,73 @@ HOW TO USE
 load up this class in a ROOT session
 $ .L $CMSSW_BASE/src/L1Trigger/L1JetEnergyCorrections/bin/local_L1JEC_scripts/tuneFits.h+
 
-$ t0.setup("fileNameToTune.root", etaNames[0])
-$ t0.redoFit(delta_xMin, delta_xMax) <--try to get a better fit
-^can repeat multiple times, must do it at least once!
+$ tuneFits t0 = tuneFits("fileNameToTune.root", 0)
+$ t0.redoFit(delta_xMin, delta_xMax) <--try to get a better fit, can repeat multiple times
 $ t0.save()
 
-loop through for t0, t1, t2 etc.
+loop through for t0, t1, t2 ... t15
+e.g.
+$ tuneFits t1 = tuneFits("fileNameToTune.root", 1)
+etc.
 
 NOTES:
--it will only save correctly if you make a change
--if you re-initialize a tuneFits object it will not save correctly, you will need to restart root session
-(close browser before initialization to avoid this)
--after you save a tuneFits objects don't make further changes...it can screw with it
--the yaxis you have set before saving will be that which is saved, so zoom in correctly
+-once you have saved a tuneFits object you cannot redoFit() or save() again, so make sure you are happy with the results before saving!
+-if you try and save a tuneFits object for an eta range already used it will not work, this is to avoid overwriting previous results
+-DO NOT work on the initial file, work on a copy in case you don't like the results and need to restart
 */
-
-// DO NOT USE THE INITIAL COPIES!!!
 
 class tuneFits{
 
+public:
+	tuneFits(const string&, const size_t&);
+	void redoFit(const double&, const double&);
+	void save();
+
+private:	
 	TFile * file;
 	TGraphErrors * graph;
 	TF1 * newFit;
+	TCanvas * c;
 	double xmin_init; // initial
 	double xmax_init;
-	double xmin_rec; // most recent
-	double xmax_rec;
-	string etaRangeLabel;	
-
-public:
-	void setup(string,string);
-	void redoFit(double,double);
-	void save();
+	double xmin_new; // most recent used in manual fit
+	double xmax_new;
+	std::vector<std::string> etaBinRange;
+	size_t etaBinLabel;
+	bool haveSaved;
 };
 
-void tuneFits::setup(string inputRootFileName, string etaRange)
+tuneFits::tuneFits(const std::string& inputRootFileName, const size_t& etaBin) :
+file(TFile::Open(inputRootFileName.c_str(), "update")),
+graph(0),
+newFit(new TF1("newFit", "[0]+[1]*TMath::Erf([2]*(log10(x)-[3])+[4]*exp([5]*(log10(x)-[6])*(log10(x)-[6])))")),
+c(new TCanvas("c","c")),
+xmin_new(0),
+xmax_new(0),
+etaBinRange({"0_0.435", "0.435_0.783", "0.783_1.131", "1.131_1.305", "1.305_1.479", "1.479_1.653", "1.653_1.83", "1.83_1.93", "1.93_2.043", "2.043_2.172", "2.172_2.322", "2.322_2.5", "2.5_2.964", "2.964_3.489", "3.489_4.191", "4.191_5.191"}),
+etaBinLabel(etaBin),
+haveSaved(false)
 {
-	file = TFile::Open(inputRootFileName.c_str(), "update");
-
-	etaRangeLabel = etaRange;
-
-	string graphNameGrab = "l1corr_eta_" + etaRangeLabel;
+	if (etaBinLabel > etaBinRange.size()-1){
+		std::cout << "@@@ whatup FOOL @@@" << std::endl;
+		std::cout << "You did not provide a valid eta bin" << std::endl;
+		std::cout << "It should be a size_t between 0 and " << etaBinRange.size()-1 << std::endl;
+		std::cout << "WARNING: Using the first eta bin by default" << std::endl;
+		etaBinLabel = 0;
+	}
+	std::string graphNameGrab = "l1corr_eta_" + etaBinRange[etaBinLabel];
 	graph = (TGraphErrors*)file->Get(graphNameGrab.c_str());
-	string graphNameUse = "l1corr_eta_" + etaRangeLabel + "_fit";
+	std::string graphNameUse = "l1corr_eta_" + etaBinRange[etaBinLabel] + "_fit";
 	graph->SetName(graphNameUse.c_str());
 
-	string fitName = "fitfcneta_" + etaRangeLabel;
+	std::string fitName = "fitfcneta_" + etaBinRange[etaBinLabel];
 	TF1 * oldFit = (TF1*)file->Get(fitName.c_str());
 	xmin_init = oldFit->GetXmin();
 	xmax_init = oldFit->GetXmax();
-	cout << endl;
-	cout << "eta range = " + etaRangeLabel << endl;
-	cout << "oldFit parameters: " << endl;
-	cout << "p0: " << oldFit->GetParameter(0) << endl;
-	cout << "p1: " << oldFit->GetParameter(1) << endl;
-	cout << "p2: " << oldFit->GetParameter(2) << endl;
-	cout << "p3: " << oldFit->GetParameter(3) << endl;
-	cout << "p4: " << oldFit->GetParameter(4) << endl;
-	cout << "p5: " << oldFit->GetParameter(5) << endl;
-	cout << "p6: " << oldFit->GetParameter(6) << endl;
-	cout << "oldFit xmin: " << xmin_init << endl;
-	cout << "oldFit xmax: " << xmax_init << endl << endl;					
+	std::cout << std::endl;
+	std::cout << "*** eta range = " + etaBinRange[etaBinLabel] << " ***" << std::endl;
+	std::cout << "below are the OldFit parameters: " << std::endl;
 
-	newFit = new TF1("newFit", "[0]+[1]*TMath::Erf([2]*(log10(x)-[3])+[4]*exp([5]*(log10(x)-[6])*(log10(x)-[6])))");
 	newFit->SetParameter(0, oldFit->GetParameter(0));
 	newFit->SetParameter(1, oldFit->GetParameter(1));
 	newFit->SetParameter(2, oldFit->GetParameter(2));
@@ -82,33 +86,39 @@ void tuneFits::setup(string inputRootFileName, string etaRange)
 	newFit->SetParameter(4, oldFit->GetParameter(4));
 	newFit->SetParameter(5, oldFit->GetParameter(5));
 	newFit->SetParameter(6, oldFit->GetParameter(6));
-	// the 'browser range' you select here will be used going forward
-	graph->Draw();
-	oldFit->Draw("same");
-	return;						
+
+	redoFit(0.0, 0.0); // the TBrowser axis ranges you select for the resulting plot will be used going forward
 }
 
 
-void tuneFits::redoFit(double posRelToXmin, double posRelToXmax)
+void tuneFits::redoFit(const double& posRelToXmin, const double& posRelToXmax)
 {
-	xmin_rec = xmin_init + posRelToXmin;
-	xmax_rec = xmax_init + posRelToXmax;
+	if (haveSaved == true){
+		std::cout << "This tuneFits object has already been saved! Cannot redo fit now! Not redo-ing fit..." << std::endl;
+		return;
+	}
+	xmin_new = xmin_init + posRelToXmin;
+	xmax_new = xmax_init + posRelToXmax;
 
-	cout << endl << "Fitting between the ranges: " << xmin_rec << " and " << xmax_rec << endl << endl;
-	graph->Fit(newFit, "", "", xmin_rec, xmax_rec);
-	cout << endl;
+	std::cout << std::endl << "Fitting between the ranges: " << xmin_new << " and " << xmax_new << std::endl << std::endl;
+	graph->Fit(newFit, "", "", xmin_new, xmax_new);
+	std::cout << std::endl;
 	graph->Draw("AL");
+	return;
 }
 
 
-// replaces the fit and graph&fit in the .root file
 void tuneFits::save()
 { 
-	// set the ranges to the fit range(ish)
-	graph->GetXaxis()->SetRangeUser(0,xmax_rec); // will use the y-axis of how you were looking at it
-	// Not as simple for a function
-	string fitName = "EDITFIT_fitfcneta_" + etaRangeLabel;
-	TF1 * saveNewFit = new TF1(fitName.c_str(), "[0]+[1]*TMath::Erf([2]*(log10(x)-[3])+[4]*exp([5]*(log10(x)-[6])*(log10(x)-[6])))", xmin_rec, xmax_rec);
+	if (haveSaved == true){
+		std::cout << "This tuneFits object has already been saved! Cannot save again! Not Saving..." << std::endl;
+		return;
+	}
+	// set the ranges to match the fit range(ish)
+	graph->GetXaxis()->SetRangeUser(0,xmax_new); // NB: will use the y-axis of how you were looking at it in TBrowser
+	// it is not as simple for a function
+	std::string fitName = "EDITFIT_fitfcneta_" + etaBinRange[etaBinLabel];
+	TF1 * saveNewFit = new TF1(fitName.c_str(), "[0]+[1]*TMath::Erf([2]*(log10(x)-[3])+[4]*exp([5]*(log10(x)-[6])*(log10(x)-[6])))", xmin_new, xmax_new);
 	saveNewFit->SetParameter(0, newFit->GetParameter(0));
 	saveNewFit->SetParameter(1, newFit->GetParameter(1));
 	saveNewFit->SetParameter(2, newFit->GetParameter(2));
@@ -117,18 +127,19 @@ void tuneFits::save()
 	saveNewFit->SetParameter(5, newFit->GetParameter(5));
 	saveNewFit->SetParameter(6, newFit->GetParameter(6));
 
-	string graphNameUse = "EDITFIT_l1corr_eta_" + etaRangeLabel + "_fit";
-	// string graphNameDel = graphNameUse + ";1";
-	// file->Delete(graphNameDel.c_str());
-	// string fitNameDel = fitName + ";1";
-	// file->Delete(fitNameDel.c_str());
+	// save with same names + EDITFIT prefix
+	std::string graphNameUse = "EDITFIT_l1corr_eta_" + etaBinRange[etaBinLabel] + "_fit";
+	// first check save object doesn't already exist
+	if ((TGraphErrors*)file->Get(graphNameUse.c_str())!=0){
+		std::cout << "The graphs you are trying to save already exist! Maybe you have accidentally used the same etaBin. Don't want to overwrite, Not Saving..." << std::endl;
+		c->Close();
+		return;		
+	}
 	graph->SetName(graphNameUse.c_str());
 	graph->Write();
 	saveNewFit->Write();
 	file->Close();
+	c->Close();
+	haveSaved = true;
+	return;
 }
-
-// remember to use these objects to help you
-// IMPORT THESE OBJECTS FOR EASY INTERACTIVE RUNNING
-string etaNames[16] = {"0_0.435", "0.435_0.783", "0.783_1.131", "1.131_1.305", "1.305_1.479", "1.479_1.653", "1.653_1.83", "1.83_1.93", "1.93_2.043", "2.043_2.172", "2.172_2.322", "2.322_2.5", "2.5_2.964", "2.964_3.489", "3.489_4.191", "4.191_5.191"}; 
-tuneFits t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15;
